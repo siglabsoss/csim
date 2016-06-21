9cd5e74ef4443ec070193b6a9b4544ca0c546567
ubuntu <daveybelliss@gmail.com>
2016-06-20 18:58:54 -0700

    added test file

---
+++ src/test.cpp
@@ -0,0 +1,15 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_AUTO_TEST_MAIN
+#define BOOST_TEST_MODULE Hello
+#include <boost/test/included/unit_test.hpp>
+ 
+int add(int i, int j)
+{
+    return i + j;
+}
+ 
+BOOST_AUTO_TEST_CASE(universeInOrder)
+{
+    BOOST_CHECK(add(2, 2) == 4);
+
+}
