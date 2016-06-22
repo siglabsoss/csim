63889d59335f23bc8016fcadb668ba46e58a054a
davey <daveybelliss@gmail.com>
2016-06-21 18:21:25 -0700

    added systemc ZOMG

---
+++ osiderived.cpp
@@ -0,0 +1,48 @@
+#include "osiderived.h"
+#include "osibase.h"
+#include "osibase.cpp"
+using namespace std;
+
+OsiDerived::OsiDerived()
+{
+
+}//OsiDerived constructor
+
+
+void OsiDerived::txdown(int* data, bool meta)
+{
+   // return down->rxup(data, meta);
+}//Transmit to lower layer
+
+void OsiDerived::txup(int* data, bool meta)
+{
+//    return up->rxdown(data,meta);
+}//transmit to upper layer
+
+ void OsiDerived::rxdown(int* data, bool meta)
+{
+        cout << "Warning." << name << " does not implement rxdown().";
+   //     return this->txup(data,meta);
+}//Receive from the lower layer  VIRTUAL
+
+
+ void OsiDerived::rxup(int* data, bool meta)
+{
+    cout << "Warning. " << name << " does not implement rxup().";
+ //   return this->txdown(data,meta);
+}//Recieve from the up layer  VIRTUAL
+
+void OsiDerived::tick()
+{}//Pass
+
+    // void set_parent(OsiDerived* parent)
+    // {
+    //     this->up = parent;
+    //     *parent->down = this;
+    // }; //Sets the parent
+
+
+OsiDerived::~OsiDerived()
+{
+
+}
