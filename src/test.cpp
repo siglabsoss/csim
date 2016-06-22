a1e41280a7a1dcd8b2a1967715c6872c73e51e4f
davey <daveybelliss@gmail.com>
2016-06-21 18:52:28 -0700

    basic sanity test of systemc int wrapping

--- src/test.cpp
+++ src/test.cpp
@@ -2,6 +2,7 @@
 #define BOOST_AUTO_TEST_MAIN
 #define BOOST_TEST_MODULE Hello
 #include <boost/test/included/unit_test.hpp>
+#include "systemc.h"
  
 int add(int i, int j)
 {
@@ -12,4 +13,30 @@ BOOST_AUTO_TEST_CASE(universeInOrder)
 {
     BOOST_CHECK(add(2, 2) == 4);
 
+}
+
+
+BOOST_AUTO_TEST_CASE(systemCMultiplyWrap)
+{
+	sc_uint<4> count = 15;
+	sc_uint<4> mul = 14;
+
+	sc_uint<4> res;
+
+	res = count * mul;
+
+	BOOST_CHECK_EQUAL(res.to_int(), 2);
+}
+
+
+BOOST_AUTO_TEST_CASE(systemCMultiplyMax)
+{
+	sc_uint<4> count = 15;
+	sc_uint<4> mul = 15;
+
+	sc_uint<8> res;
+
+	res = count * mul;
+
+	BOOST_CHECK_EQUAL(res.to_int(), 225);
 }
