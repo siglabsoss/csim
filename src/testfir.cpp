ec40df1124d1efe49af1cb3ad93e67bd9854dc90
davey <daveybelliss@gmail.com>
2016-07-01 12:16:08 -0700

    Adding empty testfir.cpp

---
+++ src/testfir.cpp
@@ -0,0 +1,33 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_AUTO_TEST_MAIN
+#define BOOST_TEST_MODULE Complex
+#include <boost/test/included/unit_test.hpp>
+#include <iostream>
+#include<cstring>
+#include <stdio.h>
+#include <iostream>     // cout, endl
+#include <vector> //For storing parsed data from file
+#include <iterator>     // ostream_operator
+#include <boost/tokenizer.hpp> //For parsing data from file
+#include <iomanip> //For setprecision
+#include "fixedfir.h"
+
+
+using namespace boost;
+using namespace std;
+
+
+BOOST_AUTO_TEST_CASE(REAL_FILTER)
+{
+
+}
+
+BOOST_AUTO_TEST_CASE(IMAGINARY_FILTER)
+{
+
+}
+
+BOOST_AUTO_TEST_CASE(COMPLEX_FILTER)
+{
+
+}
