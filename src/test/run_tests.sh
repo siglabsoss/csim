d67ecf665a33aa237282184f04b2da78db7fbef7
Behrooze Sirang <behrooze@siglabs.com>
2016-07-28 14:10:30 -0700

    test: cosmetic change

--- src/test/run_tests.sh
+++ src/test/run_tests.sh
@@ -7,8 +7,9 @@ CSIM_UTEST_BUILD=$CSIM_SRC/../csim_utest_build #define the build tree to be a si
 
 #Construct semicolon separated list of unit test source files relative to this directory
 #because that's where the CMakeLists.txt file lives.
-#XXX as the number of files grows, this logic will need to be udpated because xargs will exceed the
-#maximum number of command line arguments
+#XXX as the number of files grows, this logic may need to be udpated because the maximum
+#number of command line arguments could be exhausted although xargs supposedly handles
+#that
 
 if [ $# -eq 0 ]; then
     TEST_FILTER=utests/.*cpp
