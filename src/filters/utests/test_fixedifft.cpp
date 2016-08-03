d3d64ed18368566b7fd60328e45cce04005b706e
davey <daveybelliss@gmail.com>
2016-08-03 12:50:43 -0700

    IFFT unit test of FFT-IFFT passes using cordic.

--- src/filters/utests/test_fixedifft.cpp
+++ src/filters/utests/test_fixedifft.cpp
@@ -12,6 +12,7 @@
 
 #include <utils/utils.hpp> //reverseBits()
 #include <filters/fixedifft.hpp>
+#include <filters/fixedfft.hpp>
 
 using namespace std;
 
@@ -279,79 +280,90 @@ CSIM_TEST_CASE(IFFT_TWO_INPUTS)
 }//Checks for two consecutive sets of inputs in the same FFT.
 
 
-//CSIM_TEST_CASE(FFT_VERILOG)
-//{
-//       string infile("../csim/data/fft/output/out1.txt");
-//       string answersfile("../csim/data/fft/input/verilog.txt");
-//
-//       int i = 0;
-//       FixedComplex<32> bitReversedOutput[32768];
-//       ifstream in(infile.c_str());
-//       BOOST_REQUIRE_MESSAGE(in.is_open(), "Could not open data/fft/input/verilog.txt");
-//       ifstream ans(answersfile.c_str());
-//       BOOST_REQUIRE_MESSAGE(ans.is_open(), "Could not open ../csim/data/fft/output/out1.txt");
-//
-//       std::string token;
-//       string line;
-//       int inputs = 0;
-//       while(getline(in,line)) {
-//           istringstream ss(line);
-//           getline(ss, token, ',');
-//           stringstream strValue;
-//           strValue << token;
-//           int intValue;
-//           strValue >> intValue;
-//           bitReversedOutput[inputs].real = intValue;
-//           getline(ss, token, ',');
-//           stringstream strValue2;
-//           strValue2 << token;
-//           strValue2 >> intValue;
-//           bitReversedOutput[inputs++].imag = intValue;
-//       }//Reads in inputs from file. Parsing by commas. Format is: real,imag\n
-//
-//       int numAnswers = 0;
-//       FixedComplex<32> trueAnswers[32768];
-//       while(getline(ans,line)) {
-//
-//         istringstream ss(line);
-//         getline(ss, token, ',');
-//         stringstream strValue;
-//         strValue << token;
-//         int intValue;
-//         strValue >> intValue;
-//         trueAnswers[numAnswers].real = intValue;
-//         getline(ss, token, ',');
-//         stringstream strValue2;
-//         strValue2 << token;
-//         strValue2 >> intValue;
-//        trueAnswers[numAnswers++].imag = intValue;
-//
-//     }//Reads in inputs from file. Parsing by commas. Format is: real,imag\n
-//
-////
-////        FixedComplex<32> temp[32769];
-////        for (i = 0; i < inputs; i++) {
-////            temp[i] = bitReversedOutput[reverseBits(inputs, i)];
-////        }//Reformats data in correct order
-//
-//
-//       FixedComplex<32> temp[32769];
-//       for (i = 0; i < inputs; i++) {
-//           temp[i] = bitReversedOutput[i];
-//       }
-//
-//
-//
-//        for (i = 0; i < 10; i++) {
-//            BOOST_CHECK_MESSAGE(abs((temp[i].real - trueAnswers[i].real)/(float)trueAnswers[i].real) <  .01 || abs(abs(temp[i].real) - abs(trueAnswers[i].real)) < 10 ,
-//                    "I: " << i << " Output: " << temp[i].real << " Answer: " << trueAnswers[i].real << " Ratio: " << abs((temp[i].real - trueAnswers[i].real)/(float)trueAnswers[i].real) );
-//            BOOST_CHECK_MESSAGE(abs((trueAnswers[i].imag - temp[i].imag )/(float)trueAnswers[i].imag) < .01 || abs(abs(temp[i].imag) - abs(trueAnswers[i].imag)) < 10,
-//                    "I: " << i << " Output: " << temp[i].imag << " Answer: " << trueAnswers[i].imag << " Ratio: " << abs((temp[i].imag - trueAnswers[i].imag)/(float)trueAnswers[i].imag) );
-//
-//        }
-//
-//
-//}//Used for comparing percent error
+CSIM_TEST_CASE(FFT_IFFT)
+{
+       string infile("../csim/data/fft/input/data_file_complex1.csv");
+
+
+       int i = 0;
+       ifstream in(infile.c_str());
+       BOOST_REQUIRE_MESSAGE(in.is_open(), "Could not open " << infile);
+       FixedComplex<32> originalInput[32768];
+       std::string token;
+       string line;
+       int inputs = 0;
+       while(getline(in,line)) {
+           istringstream ss(line);
+           getline(ss, token, ',');
+           stringstream strValue;
+           strValue << token;
+           int intValue;
+           strValue >> intValue;
+           originalInput[inputs].real = intValue;
+           getline(ss, token, ',');
+           stringstream strValue2;
+           strValue2 << token;
+           strValue2 >> intValue;
+           originalInput[inputs++].imag = intValue;
+       }//Reads in inputs from file. Parsing by commas. Format is: real,imag\n
+
+       FixedComplex<32> answers[32769];//Array to store answers
+       int count = 0; //How many outputs have been collected
+
+       int points = inputs;
+       filter_io_t data;
+       data.type =  IO_TYPE_FIXED_COMPLEX_32;
+       fixedfft fft(points);
+
+       for (int i = 0; i < 2; i++) {
+           for (int j = 0; j < points; j++) {
+
+               data.fc32 = originalInput[j];
+               fft.input(data);
+               bool test = fft.output(data);
+               if (test) {
+                  answers[count++] = data.fc32;
+               }
+           }
+       }
+
+       FixedComplex<32> temp[32769];
+       for (i = 0; i < inputs; i++) {
+           temp[reverseBits(inputs, i)] = answers[i];
+       }//Reformats data in correct order
+
+       count = 0; //How many outputs have been collected
+
+          points = inputs;
+
+            data.type =  IO_TYPE_FIXED_COMPLEX_32;
+            fixedifft ifft(points);
+
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < points; j++) {
+
+                    data.fc32 = temp[j];
+                    ifft.input(data);
+                    bool test = ifft.output(data);
+                    if (test) {
+                       answers[count++] = data.fc32;
+                    }
+                }
+            }
+
+
+            for (i = 0; i < inputs; i++) {
+                temp[reverseBits(inputs, i)] = answers[i];
+            }//Reformats data in correct order
+
+
+        for (i = 0; i < inputs; i++) {
+            BOOST_CHECK_MESSAGE(abs((temp[i].real - originalInput[i].real)/(float)originalInput[i].real) <  .01 || abs(abs(temp[i].real) - abs(originalInput[i].real)) < 5 ,
+                    "I: " << i << " Output: " << temp[i].real << " Answer: " << originalInput[i].real << " Ratio: " << abs((temp[i].real - originalInput[i].real)/(float)originalInput[i].real) );
+            BOOST_CHECK_MESSAGE(abs((originalInput[i].imag - temp[i].imag )/(float)originalInput[i].imag) < .01 || abs(abs(temp[i].imag) - abs(originalInput[i].imag)) < 5,
+                    "I: " << i << " Output: " << temp[i].imag << " Answer: " << originalInput[i].imag << " Ratio: " << abs((temp[i].imag - originalInput[i].imag)/(float)originalInput[i].imag) );
+        }
+}//Used for comparing percent error
 
 
 CSIM_TEST_SUITE_END()
