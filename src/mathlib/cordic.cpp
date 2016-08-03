0e9383eeaec35aa179ec2cae17d6398fe733b5b4
Behrooze Sirang <behrooze@siglabs.com>
2016-08-03 10:24:22 -0700

    mathlib: convert cordic to sc_fixed

--- src/mathlib/cordic.cpp
+++ src/mathlib/cordic.cpp
@@ -9,53 +9,56 @@
 
 using namespace std;
 
-cordic::cordic()
+cordic::cordic() :
+        vals(NUM_HARDWIRED_VALUES),
+        sign(NUM_HARDWIRED_VALUES),
+        quad(-1)
 {
-    for (int i = 0; i < 16; i++) {
+    //See Hard-wiring the machine: http://www.qc.cuny.edu/Academics/Degrees/DMNS/Faculty%20Documents/Sultan1.pdf
+    for (int i = 0; i < NUM_HARDWIRED_VALUES; i++) {
 
-        if (i == 0)
-            vals[i] = .785398 * (1 << 15);
-        else if (i == 1)
-            vals[i] = .463648 * (1 << 15);
-        else if (i == 2)
-            vals[i] = .244979 * (1 << 15);
-        else if (i == 3)
-            vals[i] = .124355 * (1 << 15);
-        else if (i == 4)
-            vals[i] = .062419 * (1 << 15);
-        else
-            vals[i] = 1 << (15 - i); //((float)1/(1 << (i))) * (1 << 15);
-
+        if (i == 0) {
+            vals[i] = 0.78539816339744830961;
+        } else if (i == 1) {
+            vals[i] = 0.463647609;
+        } else if (i == 2) {
+            vals[i] = 0.2449786631;
+        } else if (i == 3) {
+            vals[i] = 0.1243549945;
+        } else if (i == 4) {
+            vals[i] = 0.06241881;
+        } else if (i == 5) {
+            vals[i] = 0.0312398334;
+        } else {
+            vals[i] = 1.0 / (1 << i);
+        }
     } //Calculates and sets values to rotate by
-
-    k = .607252935; //constant k
-
 }
 
-void cordic::rotate(sc_int<32> theta)
+void cordic::rotate(cordic_theta_t theta)
 {
-    while (theta >= 205888) //input greater than 2pi
-        theta = theta - 205888;
+    //std::cout << "Theta = " << theta;
+    while (theta >= cordic_theta_t(2 * M_PI)) { //input greater than 2pi
+        theta = theta - (2 * M_PI);
+    }
 
-    if (theta >= 154415) //4th quadrant
-            {
+    if (theta >= cordic_theta_t(1.5 * M_PI)) { //4th quadrant 1.5pi
         quad = 4;
-        theta = theta - 154415;
-    } else if (theta >= 102943) //3rd quadrant
-            {
+        theta = theta - (1.5 * M_PI);
+    } else if (theta >= cordic_theta_t(M_PI)) { //3rd quadrant pi
         quad = 3;
-        theta = theta - 102943;
-    } else if (theta >= 51471) //2nd quadrant
-            {
+        theta = theta - M_PI;
+    } else if (theta >= cordic_theta_t(0.5 * M_PI)) { //2nd quadrant 0.5pi
         quad = 2;
-        theta = theta - 51471;
-    } else
-        //1st quadrant
+        theta = theta - (0.5 * M_PI);
+    } else { //1st quadrant
         quad = 1;
+    }
 
-    sc_int<20> x = 0; //starting point for rotations
-    for (int i = 0; i < 16; i++) {
+    cordic_theta_t x = 0.0; //starting point for rotations
 
+    //std::cout << " in quadrant " << quad << std::endl;
+    for (int i = 0; i < NUM_HARDWIRED_VALUES; i++) {
         if (x <= theta) {
             sign[i] = 1;
             x = x + vals[i];
@@ -68,87 +71,69 @@ void cordic::rotate(sc_int<32> theta)
     }
 }
 
-sc_int<32> cordic::sin(sc_int<32> theta)
+cordic_scalar_t cordic::sin(cordic_scalar_t theta)
 {
-    FixedComplex<16> a(1);
-    FixedComplex<16> b(0);
-    sc_int<32> cosdown;
-    sc_int<32> cosup;
-    sc_int<32> sinup;
-    sc_int<32> sindown;
+    cordic_complex_t a(1.0, 0.0);
+    cordic_complex_t b(0.0, 0.0);
+    cordic_scalar_t cosdown;
+    cordic_scalar_t cosup;
+    cordic_scalar_t sinup;
+    cordic_scalar_t sindown;
 
     calculate(theta, a, b, &sinup, &sindown, &cosup, &cosdown);
     return sinup;
-
 }
 
-void cordic::calculate(sc_int<32> theta, FixedComplex<16> a, FixedComplex<16> b,
-        sc_int<32>* sinup, sc_int<32>* sindown, sc_int<32>* cosup,
-        sc_int<32>* cosdown)
+void cordic::calculate(cordic_theta_t theta, cordic_complex_t a, cordic_complex_t b,
+        cordic_scalar_t* sinup, cordic_scalar_t* sindown, cordic_scalar_t* cosup,
+        cordic_scalar_t* cosdown)
 {
     rotate(theta);
 
-    y[0][0] = (a.to_32() << 15); //scale up input
-    y[1][0] = (b.to_32() << 15); //scale up input
+    y[0] = a;
+    y[1] = b;
 
-    for (int i = 0; i < 16; i++) {
-        z[0][0].real = 1 << 15; //32768. Upper left
-        z[0][0].imag = 0;
-        z[0][1].real = -(1 << (15 - i));
-        z[0][1].imag = 0;
-        z[1][0].real = -z[0][1].real; // Lower left
-        z[1][0].imag = 0;
-        z[1][1].real = 1 << 15; // 1. Lower right
-        z[1][1].imag = 0;
+    for (int i = 0; i < NUM_HARDWIRED_VALUES; i++) {
+        double inverse_2exp = vals[i];
+        temp = y[0]; //temporary storage for later calculation
+        c1 = y[0];
+        c2 = y[1] * cordic_scalar_t(inverse_2exp);
 
-        temp = y[0][0]; //temporary storage for later calculation
-        c1 = y[0][0]; //(((z[0][0] * y[0][0]) >> 15)); z[0][0].real = 1 << 15
-        c2 = y[1][0] >> i; //(((z[0][1]) * (y[1][0].real))  >> 15); z[0][1].real = -(1<< (15 - i));
-        c2.real = -c2.real; //-(1<< (15 - i));
-        c2.imag = -c2.imag; // * -1
-
-        if (sign[i] == -1) {
-            c2.imag = -c2.imag;
-            c2.real = -c2.real; //multiply by rotation direction
+        if (sign[i] == 1) {
+            c2 = c2 * cordic_scalar_t(-1.0);
         }
 
-        y[0][0] = c1 + c2; //Upper value
-        c1 = y[1][0]; //((z[1][1] * y[1][0]) >> 15) z[1][1] = 1 << 15
-        c2 = temp >> i; //	c2 = ((z[1][0] * temp) >> 15);
+        y[0] = c1 + c2; //Upper value
+        c1 = y[1]; //((z[1][1] * y[1][0]) >> 15) z[1][1] = 1 << 15
+        c2 = temp * cordic_scalar_t(inverse_2exp);
+
         if (sign[i] == -1) {
-            c2.imag = -c2.imag;
-            c2.real = -c2.real; //multiply by rotation direction
+            c2 = c2 * cordic_scalar_t(-1.0);
         }
-        y[1][0] = c2 + c1; //Lower value
+        y[1] = c2 + c1; //Lower value
     }
 
-    y[0][0].real = (y[0][0].real * (k * (1 << 15))); //multiply by k
-    y[1][0].real = (y[1][0].real * (k * (1 << 15))); //multiply by k
-    y[0][0].imag = (y[0][0].imag * (k * (1 << 15))); //multiply by k
-    y[1][0].imag = (y[1][0].imag * (k * (1 << 15))); //multiply by k
-
-    y[0][0] = y[0][0] >> 15;
-    y[1][0] = y[1][0] >> 15;
+    y[0] = y[0] * cordic_scalar_t(k);
+    y[1] = y[1] * cordic_scalar_t(k);
 
     signs();
 
-//	cout << "Cosine: " << y[0][0].real/32768.0 << endl;
-//	cout << "Sine: " << y[1][0].real/32768.0 << endl;
+    cout << "Cosine, " << y[0].real() << ", " << theta << ", " << quad << endl;
+	//cout << "Sine, " << y[1].real() << ", " << theta << endl;
 
     for (int i = 0; i < 2; i++) {
-            if (y[i][0].real > 32768) {
-                y[i][0].real = 32768;
-            }
-            if (y[i][0].imag > 32768) {
-                y[i][0].imag = 32768;
-            }
+        if (y[i].real() > 1.0) {
+            y[i].real(1.0);
+        }
+        if (y[i].imag() > 1.0) {
+            y[i].imag(1.0);
+        }
     }
 
-
-    *cosdown = y[0][0].real;
-    *cosup = y[1][0].imag;
-    *sindown = y[0][0].imag;
-    *sinup = y[1][0].real;
+    *cosdown = y[0].real();
+    *cosup = y[1].imag();
+    *sindown = y[0].imag();
+    *sinup = y[1].real();
 
     return;
 
@@ -157,23 +142,23 @@ void cordic::calculate(sc_int<32> theta, FixedComplex<16> a, FixedComplex<16> b,
 void cordic::signs()
 {
     if (quad == 4) {
-        temp = y[0][0];
-        y[0][0] = y[1][0];
-        y[1][0].real = -temp.real;
-        y[1][0].imag = -temp.imag;
+        temp = y[0];
+        y[0] = y[1];
+        y[1] = temp * cordic_scalar_t(-1);
     } //swap sin and cosine. sin is negative
     else if (quad == 3) {
-        y[0][0].real = -y[0][0].real;
-        y[1][0].real = -y[1][0].real;
-        y[0][0].imag = -y[0][0].imag;
-        y[1][0].imag = -y[1][0].imag;
+        y[0] = y[0] * cordic_scalar_t(-1);
+        y[1] = y[1] * cordic_scalar_t(-1);
     } //both are negative
     else if (quad == 2) {
-        temp = y[0][0];
-        y[0][0].real = -y[1][0].real;
-        y[0][0].imag = -y[1][0].imag;
-        y[1][0] = temp;
-    } //swap sin and cosine. cosine is negative
+        temp = y[0];
+        y[0] = y[1] * cordic_scalar_t(-1);
+        y[1] = temp;
+    } else if (quad == 1){//swap sin and cosine. cosine is negative
+        /* do nothing */
+    } else {
+        assert(false);
+    }
 } //swaps cosine and sign or changes sign if necessary.
 
 cordic::~cordic()
--- src/mathlib/cordic.hpp
+++ src/mathlib/cordic.hpp
@@ -10,30 +10,40 @@
 
 #include <iostream>
 #include <types/fixedcomplex.hpp>
+
+typedef sc_ufixed<62, 31> cordic_theta_t;
+typedef FixedComplex2<62, 31> cordic_complex_t;
+typedef sc_fixed<62, 31> cordic_scalar_t;
+
+//typedef double cordic_theta_t;
+//typedef std::complex<double> cordic_complex_t;
+//typedef double cordic_scalar_t;
+
+
 class cordic
 {
 public:
-
     cordic();
-    void rotate(sc_int<32> theta); //Calculates which direction to rotate
-    void calculate(sc_int<32> theta, FixedComplex<16> a, FixedComplex<16> b,
-            sc_int<32>* sinup, sc_int<32>* sindown, sc_int<32>* cosup,
-            sc_int<32>* cosdown); //Calculates sine and cosine
+    void rotate(cordic_theta_t theta); //Calculates which direction to rotate
+    void calculate(cordic_theta_t theta, cordic_complex_t a, cordic_complex_t b,
+            cordic_scalar_t* sinup, cordic_scalar_t* sindown, cordic_scalar_t* cosup,
+            cordic_scalar_t* cosdown); //Calculates sine and cosine
     void signs(); //swaps cosine and sign or changes sign if necessary
-    sc_int<32> sin(sc_int<32> theta);
+    cordic_scalar_t sin(cordic_scalar_t theta);
     virtual ~cordic();
 
-    int vals[16]; //Holds rotation values
-    int sign[16]; //Holds which direction to rotate
-
+    std::vector <cordic_scalar_t > vals;
+    std::vector <int> sign;
     int quad;
-    double k; //Constant
-    FixedComplex<32> y[2][1]; //right aray to multiply (2x1)
-    FixedComplex<32> z[2][2]; //left matrixto multiply by(2x2)
-    FixedComplex<32> temp; //temporary storage for later calculation
-    FixedComplex<32> c1; //a-(1/2^n)b
-    FixedComplex<32> c2; //a(1/2^n)+b
 
+    cordic_complex_t y[2]; //right aray to multiply (2x1)
+    cordic_complex_t temp; //temporary storage for later calculation
+    cordic_complex_t c1; //a-(1/2^n)b
+    cordic_complex_t c2; //a(1/2^n)+b
+
+private:
+    static constexpr size_t NUM_HARDWIRED_VALUES = 16;
+    static constexpr double k = .607252935; //constant k (using k for 40 values despite actually using less in this implementation as it was found to provide better results)
 };
 
 #endif /* CORDIC_H_ */
