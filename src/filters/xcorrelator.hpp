abfd19076e281c2d860cd6bbdb33335bf47c533e
davey <daveybelliss@gmail.com>
2016-08-08 18:18:48 -0700

    xcorrelator works again.

--- src/filters/xcorrelator.hpp
+++ src/filters/xcorrelator.hpp
@@ -1,27 +1,27 @@
-///*
-// * XCorrelator.hpp
-// *
-// *  Created on: Jul 28, 2016
-// *      Author: ubuntu
-// */
-//
-//#pragma once
-//#include <filters/fixedfft.hpp>
-//#include <filters/fixedifft.hpp>
-//#include <vector>
-//#include <utils/utils.hpp>
-//#include <queue>
-//
-//class XCorrelator
-//{
-//public:
-//    XCorrelator(int N);
-//    vector<FixedComplex<32> > xCorrelate(vector<FixedComplex<32> > x,vector<FixedComplex<32> > y); //cross correlates 2 vectors of FixedComplex<32>
-//    vector<FixedComplex<32> > fft(vector<FixedComplex<32> > vals);//computes fft
-//    vector<FixedComplex<32> > ifft(vector<FixedComplex<32> > vals);//computes ifft
-//    vector<FixedComplex<32> > fftshift(vector<FixedComplex<32> > vals);//fftshifts the results
-//
-//    int m_n;// n point fft and ifft
-//
-//};
-//
+/*
+ * XCorrelator.hpp
+ *
+ *  Created on: Jul 28, 2016
+ *      Author: ubuntu
+ */
+
+#pragma once
+#include <filters/fixedfft.hpp>
+#include <filters/fixedifft.hpp>
+#include <vector>
+#include <utils/utils.hpp>
+#include <queue>
+
+class XCorrelator
+{
+public:
+    XCorrelator(int N);
+    vector<FixedComplex<32> > xCorrelate(vector<FixedComplex<32> > x,vector<FixedComplex<32> > y); //cross correlates 2 vectors of FixedComplex<32>
+    vector<FixedComplex<32> > fft(vector<FixedComplex<32> > vals);//computes fft
+    vector<FixedComplex<32> > ifft(vector<FixedComplex<32> > vals);//computes ifft
+    vector<FixedComplex<32> > fftshift(vector<FixedComplex<32> > vals);//fftshifts the results
+
+    int m_n;// n point fft and ifft
+
+};
+
