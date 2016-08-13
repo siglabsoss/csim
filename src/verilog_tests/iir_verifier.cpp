f8a6dd05b68a560aa0e221baa6dbd24001bebf0d
davey <daveybelliss@gmail.com>
2016-08-12 18:40:29 -0700

    iir_verifier uses complexRead16Scaled

--- src/verilog_tests/iir_verifier.cpp
+++ src/verilog_tests/iir_verifier.cpp
@@ -16,79 +16,46 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
-	string data(argv[1]); //Input data file
-	ifstream in(data.c_str());
-	if (!in.is_open()) {
-		cout << "Could not read from " << data;
-	}
-
-	string outfile(argv[2]);
-	ofstream out(outfile.c_str());
-	if (!out.is_open()) {
-		cout << "Could not read from " << outfile;
-	}
-
-	string taps(argv[3]);
-	ifstream in2(taps.c_str());
-	if (!in2.is_open()) {
-		cout << "Could not read from " << taps;
-	}
-
-	string taps2(argv[4]);
-	ifstream in4(taps2.c_str());
-	if (!in4.is_open()) {
-		cout << "Could not read from " << taps2;
-	}
-
-	vector<FixedComplex16> input(32768); //Array to hold inputs
-	vector<FixedComplex16> output(32768); //Array to hold outputs
+	vector<FixedComplex16> input; //Array to hold inputs
+	vector<FixedComplex16> output; //Array to hold outputs
 	double realAnswers[1024]; //Array to hold answers
 	double imagAnswers[1024]; //Arrayto hodl answers
-	vector<FixedComplex16> atap(100); //Array for A taps
-	vector<FixedComplex16> btap(100);
+	vector<FixedComplex16> atap; //Array for A taps
+	vector<FixedComplex16> btap;
 
+	string inFile(argv[1]); //Input data file
+	string outFile(argv[2]);
+	string taps(argv[3]);
+	string taps2(argv[4]);
 
-	char ** ptr;
-	typedef tokenizer<escaped_list_separator<char> > Tokenizer;
-	vector<string> vec;
-	string line;
-	int i = 0; //Number of inputs
-	while (getline(in, line)) {
-		Tokenizer tok(line);
-		vec.assign(tok.begin(), tok.end());
-		input[i].real(atof(vec[0].c_str())/32768.0);
-		input[i].imag(atof(vec[1].c_str())/32768.0);
-		i++;
-	} //Gets each line of data. Stores real and imaginary parts separate in FixedComplex. i stores total number of inputs.
+	input = complexRead16Scaled(inFile);
+	if (!input.empty()) {
+		cout << "Could not read from " << inFile << endl;
+		return 1;
+	}
 
-	int j = 0; //Number of A taps
-	while (getline(in2, line)) {
+	atap = complexRead16Scaled(taps);
+	if (!atap.empty()) {
+		cout << "Could not read from " << taps << endl;
+		return 1;
+	}//Gets A taps
 
-		Tokenizer tok(line);
-		vec.assign(tok.begin(), tok.end());
-		atap[j].real(atof(vec[0].c_str())/32768.0);
-		j++;
-	} //Reads in A taps
-
-	int m = 0; // Number of B taps
-	while (getline(in4, line)) {
-		Tokenizer tok(line);
-		vec.assign(tok.begin(), tok.end());
-		btap[m].real(atof(vec[0].c_str())/32768.0);
-		m++;
-	} //Reads in B taps
-
+	btap = complexRead16Scaled(taps2);
+	if (!btap.empty()) {
+		cout << "Could not read from " << taps2 << endl;
+		return 1;
+	}//Gets A taps
 
 	fixediir iir(atap, btap);
 	filter_io_t data2;
-	for (int j = 0; j < i; j ++) {
+	for (int j = 0; j < input.size(); j ++) {
 		data2 = input[j];
 		iir.input(data2); //Filters data
 		output[j] = iir.m_output;
 	}
 
-	for (int k = 0; k < i; k++) {
-	//	cout << output[k].real().range().to_int64() << " " << output[k].imag().range().to_int64() << endl;
+	ofstream out(outFile);
+	for (int k = 0; k < output.size(); k++) {
     	out << setw(6) << setfill(' ') <<  output[k].real().range().to_int64() << ",";
     	out << setw(6) << setfill(' ') <<  output[k].imag().range().to_int64() << endl;
 	}
