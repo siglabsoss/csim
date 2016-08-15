3d8bd5847e08e86228df2b4d589e5b7886bdedb7
Behrooze Sirang <behrooze@siglabs.com>
2016-08-15 15:13:42 -0700

    some refactoring of initialization logic

--- src/apps/retrosim.cpp
+++ src/apps/retrosim.cpp
@@ -12,6 +12,7 @@
 #include <filters/zero_pad_interpolator.hpp>
 #include <filters/linear_gain_amplifier.hpp>
 #include <probes/sample_count_trigger.hpp>
+#include <probes/tick_count_trigger.hpp>
 #include <probes/level_trigger.hpp>
 
 #include <sys/time.h>
@@ -32,52 +33,80 @@ timespec timediff(timespec start, timespec end)
     return temp;
 }
 
+static constexpr size_t FFT_WINDOW_SIZE = 1024;
+static constexpr size_t UPSAMPLE_FACTOR = 2;
+static constexpr size_t MOD_TICKS_PER_SYMBOL = 2;
+static constexpr size_t MIXER_TICKS_PER_PERIOD = (MOD_TICKS_PER_SYMBOL / UPSAMPLE_FACTOR) * 10;
+
+void constructModulationChain(FilterChain &modChain, bool withTriggers)
+{
+    Modulator           *qam16    = new Modulator(MOD_TICKS_PER_SYMBOL, Modulator::MOD_SCHEME_QAM16);
+    fixedfft            *fft      = new fixedfft(FFT_WINDOW_SIZE, 0);
+    ZeroPadInterpolator *zpi      = new ZeroPadInterpolator(FFT_WINDOW_SIZE, UPSAMPLE_FACTOR);
+    LinearGainAmplifier *lga      = new LinearGainAmplifier(UPSAMPLE_FACTOR);
+    fixedifft           *ifft     = new fixedifft(FFT_WINDOW_SIZE*UPSAMPLE_FACTOR, 0);
+    Mixer *upmixer              = new Mixer(MIXER_TICKS_PER_PERIOD, true /* upmix */);
+
+    //modulation_chain = *tp3 + *upmixer + *tp2 +  *ifft + *lga + *zpi + *lt1 +  *fft + *tp4 + *qam16;
+    if (withTriggers) {
+        TickCountTrigger  *lt1      = new TickCountTrigger("FFT trigger", FFT_WINDOW_SIZE*10, 1, FFT_WINDOW_SIZE*4);
+        TickCountTrigger  *tp2      = new TickCountTrigger("IFFT trigger", FFT_WINDOW_SIZE*10, 1, FFT_WINDOW_SIZE*4);
+        TickCountTrigger  *tp3      = new TickCountTrigger("Mixer trigger", FFT_WINDOW_SIZE*10, 1, FFT_WINDOW_SIZE*4);
+        TickCountTrigger  *tp4      = new TickCountTrigger("QAM trigger", FFT_WINDOW_SIZE*10, 1, FFT_WINDOW_SIZE*4);
+        TickCountTrigger  *tp5      = new TickCountTrigger("ZPI trigger", FFT_WINDOW_SIZE*10, 1, FFT_WINDOW_SIZE*4);
+        TickCountTrigger  *tp6      = new TickCountTrigger("LGA trigger", FFT_WINDOW_SIZE*10, 1, FFT_WINDOW_SIZE*4);
+        modChain = *tp3 + *upmixer + *tp2 + *ifft + *tp6 + *lga + *tp5 + *zpi + *lt1 + *fft + *tp4 + *qam16;
+    } else {
+        modChain = *upmixer + *ifft + *lga + *zpi + *fft + *qam16;
+    }
+}
+
+void constructDemodulationChain(FilterChain &demodChain, bool withTriggers)
+{
+    Mixer *downmixer = new Mixer(10, false /* downmix */);
+    TickCountTrigger *lt2  = new TickCountTrigger("Downmix", FFT_WINDOW_SIZE*10, 1, FFT_WINDOW_SIZE*5);
+    downmixer->shouldPublish(true);
+
+    if (withTriggers) {
+        demodChain = *lt2 + *downmixer;
+    } else {
+        demodChain = *downmixer;
+    }
+}
+
 void constructRadios(SigWorld &world)
 {
-    for (int i = 0; i < 1; i++) {
-        world.addRadio([i]()
-                {
-                    radio_config_t config {
-                        .position = Vector2d(0.0, i*1199.6*10),
-                        .id = static_cast<radio_id_t>(i + 1)
-                    };
+    world.addRadio([]()
+            {
+                radio_config_t config {
+                    .position = Vector2d(0.0, 0.0),
+                    .id = static_cast<radio_id_t>(0)
+                };
 
-                    constexpr size_t FFT_WINDOW_SIZE = 1024;
+                FilterChain modulation_chain;
+                constructModulationChain(modulation_chain, false);
 
-                    FilterChain modulation_chain;
-                    Modulator           *qam16    = new Modulator(Modulator::MOD_SCHEME_QAM16);
-                    fixedfft            *fft      = new fixedfft(FFT_WINDOW_SIZE, 0);
-                    ZeroPadInterpolator *zpi      = new ZeroPadInterpolator(FFT_WINDOW_SIZE, 2);
-                    LinearGainAmplifier *lga      = new LinearGainAmplifier(2);
-                    fixedifft           *ifft     = new fixedifft(FFT_WINDOW_SIZE*2, 0);
-                    //SampleCountTrigger  *tp1      = new SampleCountTrigger("FFT trigger", 1000, 1, 10000);
-                    LevelTrigger        *lt1      = new LevelTrigger("FFT Level", 1000, 10);
-                    SampleCountTrigger  *tp2      = new SampleCountTrigger("IFFT trigger", 1000, 1, 10000);
-                    SampleCountTrigger  *tp3      = new SampleCountTrigger("Mixer trigger", 1000, 1, 8000);
-                    SampleCountTrigger  *tp4      = new SampleCountTrigger("QAM trigger", 1000, 1, 15000);
+                FilterChain demodulation_chain;
+                constructDemodulationChain(demodulation_chain, false);
 
-                    //XXX use parameter for frequency
-                    Mixer *mixer                = new Mixer(3);
+                return std::unique_ptr<RadioS>(new RadioS(config, modulation_chain, demodulation_chain));
+            });
+    world.addRadio([]()
+            {
+                radio_config_t config {
+                    .position = Vector2d(0.0, 10*11.996),
+                    .id = static_cast<radio_id_t>(1)
+                };
 
-                    /*
-                    qam16->shouldPublish(true);
-                    fft->shouldPublish(true);
-                    zpi->shouldPublish(true);
-                    lga->shouldPublish(true);
-                    ifft->shouldPublish(true);
-                    mixer->shouldPublish(true);
-                    */
+                FilterChain modulation_chain;
+                constructModulationChain(modulation_chain, false);
 
-                    modulation_chain = *tp3 + *mixer + *tp2 +  *ifft + *lga + *zpi + *lt1 +  *fft + *tp4 + *qam16;
+                FilterChain demodulation_chain;
+                constructDemodulationChain(demodulation_chain, false);
 
-                    FilterChain demodulation_chain;
-                    AutomaticGain *ag = new AutomaticGain();
-                    //ag->shouldPublish(true);
-                    demodulation_chain = *ag;
 
-                    return std::unique_ptr<RadioS>(new RadioS(config, modulation_chain, demodulation_chain));
-                });
-    }
+                return std::unique_ptr<RadioS>(new RadioS(config, modulation_chain, demodulation_chain));
+            });
 }
 
 int main(int argc, char *argv[])
