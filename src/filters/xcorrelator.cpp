2513442a33656abfcfb2b32a8ad3973768356faa
davey <daveybelliss@gmail.com>
2016-08-16 13:21:04 -0700

    xcorrelator works with FixedComplex 32 as <64,49>.

--- src/filters/xcorrelator.cpp
+++ src/filters/xcorrelator.cpp
@@ -11,101 +11,86 @@ using namespace std;
 
 XCorrelator::XCorrelator(int N) :
 		m_n(N)
+{}
+
+vector<FixedComplex32 > XCorrelator::xCorrelate(vector<FixedComplex32 > x, vector<FixedComplex32 > y)
 {
-
-}
-
-vector<FixedComplex<32> > XCorrelator::xCorrelate(vector<FixedComplex<32> > x, vector<FixedComplex<32> > y)
-{
-
-    vector<FixedComplex<32> > p1 = fft(x);//output is FFT(x)
-    vector<FixedComplex<32> > p2 = fft(y);//output is FFT(y)
+    vector<FixedComplex32 > p1 = fft(x);//output is FFT(x)
+    vector<FixedComplex32 > p2 = fft(y);//output is FFT(y)
     assert(p1.size() == m_n); //Same number of outputs into FFT as inputs
     assert(p2.size() == m_n); //Same number of outputs into FFT as inputs
 
     for (int i = 0; i < p2.size(); i++) {
-        p2[i].imag = -p2[i].imag;
+        p2[i].imag(p2[i].imag() * -1);
     }//conj(FFT(Y)))
-    FixedComplex<64> debug;
     for (int i = 0; i < p1.size(); i++) {
-        debug = p1[i].to_64() * p2[i].to_64();
         p1[i] = p1[i] * p2[i];
-        if (debug.real > p1[i].to_64().real) {
-            cout << "OVERFLOW" << endl;
-        }
-        if (debug.imag > p1[i].to_64().imag) {
-            cout << "OVERFLOW" << endl;
-        }
     }//FFT(x) * conj(FFT(Y))
 
-    p1 = ifft(p1);// IFFT(FFT(x) * conj(FFT(Y)))
+    p1 = ifft(p1);// IFFT(FFT(x) * conj(FFT(y)))
 
     return fftshift(p1);
 }
 
-vector<FixedComplex<32> > XCorrelator::fft(vector<FixedComplex<32> > vals)
+vector<FixedComplex32 > XCorrelator::fft(vector<FixedComplex32 > vals)
 {
     fixedfft fft(m_n);
-    vector<FixedComplex<32> > bitReversedAnswers;
-    vector<FixedComplex<32> > answers;
-    filter_io_t data;
-    data.type =  IO_TYPE_FIXED_COMPLEX_32;
-    data.fc32 = FixedComplex<32>(0,0);
+    vector<FixedComplex32 > outputs;
+    vector<FixedComplex32 > outputsBitReversed;
 
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < m_n; j++) {
-            data.fc32 = vals[j];
-            fft.input(data);
-            bool test = fft.output(data);
-            if (test) {
-                bitReversedAnswers.push_back(data.fc32);
-            }//If an output is ready
-        }//For every input
-    }//run twice to get all outputs
+    filter_io_t data;
+    	for (int i = 0; i < 2; i++) {
+    		for (int j = 0; j < m_n; j++) {
+    			data = vals[j];
+    			fft.input(data);
+    			bool test = fft.output(data);
+    			if (test) {
+    				outputsBitReversed.push_back(data.fcn32);
+    			}//If output is ready
+    		}//Insert all input
+    	}//Insert input again to get output
 
     for (int i = 0; i < m_n; i++) {
-             answers.push_back(bitReversedAnswers[reverseBits(m_n, i)]);
+             outputs.push_back(outputsBitReversed[reverseBits(m_n, i)]);
      }//Reformats data in correct order
 
-    return answers;
+    return outputs;
 }
 
-vector<FixedComplex<32> > XCorrelator::ifft(vector<FixedComplex<32> > vals)
+vector<FixedComplex32 > XCorrelator::ifft(vector<FixedComplex32> vals)
 {
-    fixedifft ifft(m_n);
-    vector<FixedComplex<32> > bitReversedAnswers;
-    vector<FixedComplex<32> > answers;
-    filter_io_t data;
-    data.type =  IO_TYPE_FIXED_COMPLEX_32;
-    data.fc32 = FixedComplex<32>(0,0);
+	fixedifft ifft(m_n);
+	vector<FixedComplex32 > outputs;
+	vector<FixedComplex32 > outputsBitReversed;
 
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < m_n; j++) {
-            data.fc32 = vals[j];
-            ifft.input(data);
-            bool test = ifft.output(data);
-            if (test) {
-                bitReversedAnswers.push_back(data.fc32);
-            }//If an output is ready
-        }//For every input
-    }//run twice to get all outputs
+	filter_io_t data;
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < m_n; j++) {
+			data = vals[j];
+			ifft.input(data);
+			bool test = ifft.output(data);
+			if (test) {
+				outputsBitReversed.push_back(data.fcn32);
+			}//If output is ready
+		}//Insert all input
+	}//Insert input again to get output
 
-    for (int i = 0; i < m_n; i++) {
-             answers.push_back(bitReversedAnswers[reverseBits(m_n, i)]);
-     }//Reformats data in correct order
+	for (int i = 0; i < m_n; i++) {
+		outputs.push_back(outputsBitReversed[reverseBits(m_n, i)]);
+	}//Reformats data in correct order
 
-    return answers;
+	return outputs;
 }
 
 
-vector<FixedComplex<32> > XCorrelator::fftshift(vector<FixedComplex<32> > vals)
+vector<FixedComplex32 > XCorrelator::fftshift(vector<FixedComplex32 > vals)
 {
     int size = vals.size() >> 1; //size/2
     if (vals.size() % 2 == 1) {
         size++; //Take ceiling
     }
 
-    FixedComplex<32> temp;//to hold swap
+    FixedComplex32 temp;//to hold swap
 
     for (int i = 0; i < size; i++) {
         temp = vals[i];
--- src/filters/xcorrelator.hpp
+++ src/filters/xcorrelator.hpp
@@ -16,10 +16,10 @@ class XCorrelator
 {
 public:
     XCorrelator(int N);
-    vector<FixedComplex<32> > xCorrelate(vector<FixedComplex<32> > x,vector<FixedComplex<32> > y); //cross correlates 2 vectors of FixedComplex<32>
-    vector<FixedComplex<32> > fft(vector<FixedComplex<32> > vals);//computes fft
-    vector<FixedComplex<32> > ifft(vector<FixedComplex<32> > vals);//computes ifft
-    vector<FixedComplex<32> > fftshift(vector<FixedComplex<32> > vals);//fftshifts the results
+    vector<FixedComplex32 > xCorrelate(vector<FixedComplex32 > x,vector<FixedComplex32 > y); //cross correlates 2 vectors of FixedComplex<32>
+    vector<FixedComplex32 > fft(vector<FixedComplex32 > vals);//computes fft
+    vector<FixedComplex32 > ifft(vector<FixedComplex32 > vals);//computes ifft
+    vector<FixedComplex32 > fftshift(vector<FixedComplex32 > vals);//fftshifts the results
 
     int m_n;// n point fft and ifft
 
