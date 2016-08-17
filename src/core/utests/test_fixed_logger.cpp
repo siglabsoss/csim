f426bf730e3fc373418ae6108ee0680d6e2b7ff8
davey <daveybelliss@gmail.com>
2016-08-16 18:58:42 -0700

    logger checks for overflow flag.

--- src/core/utests/test_fixed_logger.cpp
+++ src/core/utests/test_fixed_logger.cpp
@@ -9,12 +9,16 @@ CSIM_TEST_SUITE_BEGIN(Fixed_Logger)
 
 CSIM_TEST_CASE(Number_Of_Operations)
 {
-	FixedPoint <4, 1>  v(0);
-	for (int i = 0; i < 1; i++)
+	FixedPoint <4, 4>  v(1);
+	cout << "Value: " << v.range().to_int64() << endl;
+	cout << "Flag: " << v.overflow_flag() << endl << endl;
+	for (int i = 0; i < 8; i++)
 	{
-		v + v;
+		v = v + v;
+		cout << "Value: " << v.range().to_int64() << endl;
+			cout << "Flag: " << v.overflow_flag() << endl << endl;
 	}
-	v.overflow_flag();
+
 	for (int i = 0; i < 3; i++)
 	{
 		v - v;
@@ -31,9 +35,19 @@ CSIM_TEST_CASE(Number_Of_Operations)
 	}
 
 	#ifdef FIXED_POINT_PROFILER_ENABLE
-		sc_dt::scfx_rep::printOperations();
+		sc_dt::scfx_rep::printLog();
+		BOOST_CHECK(sc_dt::scfx_rep::additions == 20);
+		BOOST_CHECK(sc_dt::scfx_rep::subtractions == 3);
+		BOOST_CHECK(sc_dt::scfx_rep::multiplications == 3);
+		BOOST_CHECK(sc_dt::scfx_rep::divisions == 7);
 		sc_dt::scfx_rep::clearOperations();
-		sc_dt::scfx_rep::printOperations();
+		sc_dt::scfx_rep::printLog();
+		BOOST_CHECK(sc_dt::scfx_rep::additions == 0);
+		BOOST_CHECK(sc_dt::scfx_rep::subtractions == 0);
+		BOOST_CHECK(sc_dt::scfx_rep::multiplications == 0);
+		BOOST_CHECK(sc_dt::scfx_rep::divisions == 0);
+
+
 	#endif
 
 
