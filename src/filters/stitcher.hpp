d2c100ac0693f6e9fb87a190daffc805c359986d
davey <daveybelliss@gmail.com>
2016-08-25 17:18:01 -0700

    Dusting off stitcher.cpp

--- src/filters/stitcher.hpp
+++ src/filters/stitcher.hpp
@@ -18,16 +18,30 @@
 class Stitcher
 {
 public:
+	cordic c;
+	sc_int<32> cosdown;
+	sc_int<32> cosup;
+	sc_int<32> sinup;
+	sc_int<32> sindown;
+	sc_int<32> theta; //How many radians to run a certain wave
+	sc_int<32> startingTheta; //Keeps track of original position
+	sc_int<32> t; //Total time that wave is used
+	sc_int<32> delta; //Total angle that wave is used in radians
+	sc_int<32> endTheta; // Angle to end on
+    FixedComplex<16> a1; //coefficient for cordic
+    FixedComplex<16> b1; //coefficient for cordic
+	int counter = 0; //Which sample of actual data you are on
+	sc_int<32> currentTheta; //Current angle in radians
     Stitcher(int* waveNums, int* samples, int numSections); //Takes in array of corresponding wave number,number of samples for each wave number, and number of sections
-    vector<FixedComplex<32> > stitch(int numSamples, int sampleRate,
-            int frequency, vector<FixedComplex<32> > data); //stitches data based on inputs given. Returns as a vector of fixedcomplex
+    std::vector<FixedComplex<32> > stitch(int numSamples, int sampleRate,
+            int frequency, std::vector<FixedComplex<32> > data); //stitches data based on inputs given. Returns as a vector of fixedcomplex
 
     int* val; //which waves to use at which section
     int numSections; //number of sections of output
     int* samples; //array that shows how many samples of the final output will be that wave
     int sample_total;
-    vector<FixedComplex<32> > output; //data outs
-
+    std::vector<FixedComplex<32> > output; //data outs
+    void doStuff(int val, int i, std::vector<FixedComplex<32> > data);
 };
 
 #endif /* STITCHER_H_ */
--- src/filters/utests/test_stitch.cpp
+++ src/filters/utests/test_stitch.cpp
@@ -8,70 +8,79 @@
 #include <boost/tokenizer.hpp> //For parsing data from file
 #include <iomanip> //For setprecision
 #include <stdlib.h>
-
+#include <utils/utils.hpp>
 #include <filters/stitcher.hpp>
 
 using namespace std;
 using namespace boost;
 
 CSIM_TEST_SUITE_BEGIN(StitcherFilter)
-#if 0
+
+void writeToFile(vector<FixedComplex<32> > outdatas)
+{
+	int size = outdatas.size();
+	string outFile = "data/stitcher/output/stitching.txt"; //Output data file
+	ofstream out(outFile.c_str());
+	if (!out.is_open()) {
+		   cout << "error reading" << endl;
+		   BOOST_REQUIRE_MESSAGE(0 == 1, "Could not write to data/stitcher/output/stitching.txt");
+	}
+
+	for (int i = 0; i < size; i++) {
+		out << outdatas[i].real / 32768.0 << "," << outdatas[i].imag / 32768.0 << endl;
+	}
+
+}
+
+std::vector<double> readFromFile(std::string inFile)
+{
+	char ** ptr;
+	typedef tokenizer<escaped_list_separator<char> > Tokenizer;
+
+	vector<string> vec;
+	vector<double> answers; //Vector to hold answer data read from file
+	string line;
+	string data2 = inFile; //Answers data file
+	ifstream in2(data2.c_str());
+
+	BOOST_REQUIRE_MESSAGE(in2.is_open(), "Could not read from " << inFile);
+
+	int l = 0;
+	while (getline(in2, line)) {
+	Tokenizer tok(line);
+	vec.assign(tok.begin(), tok.end());
+	answers.push_back(atof(vec[0].c_str()));
+	l++;
+	} //Gets each line of data. Allows separation of real and imaginary in future. Currently only real.
+	return answers;
+}
+
 CSIM_TEST_CASE(PYTHON_COMPARISON)
 {
-    int waves[2] = { 1, 0 };
-    int samples[2] = { 200, 200 };
-    int num_sections = sizeof(samples) / sizeof(samples[0]);
-    vector<FixedComplex<32> > datas;
-    for (int k = 0; k < 7; k++)
-        for (int i = 0; i < 32768; i++) {
-            FixedComplex<32> j(i, i);
-            datas.push_back(j);
-        }
-
-    Stitcher stitch(waves, samples, num_sections); //Creates stitcher
-    vector<FixedComplex<32> > outdatas; //Vector for output data
-    outdatas = stitch.stitch(400, 1000, 100, datas); //(int numsamples, int sampleRate, int frequency) stitches data with clockup and clockdown waves
-
-    int size = outdatas.size();
-    string outFile = "data/stitcher/output/stitching.txt"; //Output data file
-    ofstream out(outFile.c_str());
-    if (!out.is_open()) {
-           cout << "error reading" << endl;
-           BOOST_REQUIRE_MESSAGE(0 == 1, "Could not write to data/stitcher/output/stitching.txt");
-    }
-
-    for (int i = 0; i < size; i++) {
-        out << outdatas[i].real / 32768.0 << "," << outdatas[i].imag / 32768.0
-                << endl;
-        //	cout << outdatas[i];
-    }
-
-
-    char ** ptr;
-    typedef tokenizer<escaped_list_separator<char> > Tokenizer;
-
-    vector<string> vec;
-    vector<double> answers; //Vector to hold answer data read from file
-    string line;
-    string data2("data/stitcher/answers/answers1.txt"); //Answers data file
-    ifstream in2(data2.c_str());
-    if (!in2.is_open()) {
-        cout << "error reading" << endl;
-        BOOST_REQUIRE_MESSAGE(0 == 1, "Could not read from data/stitcher/answers/answers1.txt");
-    }
-
-    int l = 0;
-    while (getline(in2, line)) {
-        Tokenizer tok(line);
-        vec.assign(tok.begin(), tok.end());
-        answers.push_back(atof(vec[0].c_str()));
-        l++;
-    } //Gets each line of data. Allows separation of real and imaginary in future. Currently only real.
-
-    for (int i = 0; i < 200; i++) {
-        BOOST_CHECK_MESSAGE(abs(outdatas[i].real / 32768.0 - answers[i]) < .005,
-                i << ": " << outdatas[i].real/32768.0 << " is not equal to " << answers[i]);
-    } //Checks output data and answers to verify they are within .005 of each other
+//    int waves[2] = { 1, 0 };
+//    int samples[2] = { 200, 200 };
+//    int num_sections = sizeof(samples) / sizeof(samples[0]);
+//    vector<FixedComplex<32> > datas;
+//    int m = 32768;
+//	for (int i = 0; i < 400; i++) {
+//
+//		FixedComplex<32> j(m, m);
+//		datas.push_back(j);
+//		m*=-1;
+//	}
+//
+//    Stitcher stitch(waves, samples, num_sections); //Creates stitcher
+//    vector<FixedComplex<32> > outdatas; //Vector for output data
+//    outdatas = stitch.stitch(400, 1000, 100, datas); //(int numsamples, int sampleRate, int frequency) stitches data with clockup and clockdown waves
+//
+//    writeToFile(outdatas); //to view later
+//
+//    vector<double> answers = readFromFile("data/stitcher/answers/answers1.txt");
+//
+//    for (int i = 0; i < 400; i++) {
+//        BOOST_CHECK_MESSAGE(abs(outdatas[i].real / 32768.0 - answers[i]) < .005,
+//                i << ": " << outdatas[i].real/32768.0 << " is not equal to " << answers[i]);
+//    } //Checks output data and answers to verify they are within .005 of each other
 
 } //Compares python data with stitcher output data.
 
@@ -110,6 +119,5 @@ CSIM_TEST_CASE(CONCATENATION)
 
 
 } //Ensures 2 waves of the same type concatenated is the same as one long wave of the same type
-#endif
 
 CSIM_TEST_SUITE_END()
