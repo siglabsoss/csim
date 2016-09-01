4ab02c2e97400910f6ccedbcc84ffa609a5e6661
davey <daveybelliss@gmail.com>
2016-09-01 12:18:25 -0700

    Code cleanup. All tests pass.

--- src/filters/fixed_cic.hpp
+++ src/filters/fixed_cic.hpp
@@ -21,29 +21,23 @@ class fixedcic : public FilterChainElement
 public:
 
     bool input(const filter_io_t &data) override;
-    /**
-     * output - provide an output sample to the caller.
-     * @return false if no output sample is available.
-     */
     bool output(filter_io_t &data) override;
-
     void tick() override;
-    fixedcic();
     fixedcic(int R, int aregs, int bregs); //Constructor takes in decimation factor and number of regs on each side.
     void cic(FixedComplex16 &input); //actual filtering of data. Returns number of outputs
-    FixedComplex16 integrate(FixedComplex16 current); //integrate side of filter
-    FixedComplex16 comb(FixedComplex16 current); //comb side of filter
+    FixedComplex16 integrate(FixedComplex16 current); //integrate side of filter (+)
+    FixedComplex16 comb(FixedComplex16 current); //comb side of filter (-)
     bool downsample(); //returns false when the sample should be passed
     void reset();
 
-    bool 						goodOutput; //indicates when output is ready
-    int                         m_r; //Decimation factor
-    int                         m_numBRegisters; //number of b registers
-    int                         m_numARegisters; //number of a registers
-    int                         m_samples; //How many samples have been processed
-    std::vector<FixedComplex16>   m_a; //a registers
-    std::vector<FixedComplex16>   m_b; //b registers
-    FixedComplex16            m_output;
+    bool 							goodOutput; //indicates when output is ready
+    int                         	m_r; //Decimation factor
+    int                         	m_numBRegisters; //number of b registers
+    int                         	m_numARegisters; //number of a registers
+    int                   			m_samples; //How many samples have been processed
+    std::vector<FixedComplex16> 	m_a; //a registers
+    std::vector<FixedComplex16>  	m_b; //b registers
+    FixedComplex16          		m_output;
 };
 
 #endif /* CIC_H_ */
