68557f9315505d92fd3b9a045bd1daab590986ff
Ben Morse <bjmorse@gmail.com>
2016-09-12 16:42:59 -0700

    cleaner

--- src/filters/utests/test_ldpc.cpp
+++ src/filters/utests/test_ldpc.cpp
@@ -5,7 +5,6 @@
 
 #include <boost/tokenizer.hpp> //For parsing data from file
 
-
 CSIM_TEST_SUITE_BEGIN(LDPCFunctionality)
 
 //CSIM_TEST_CASE(LDPC_Basic)
@@ -17,54 +16,27 @@ CSIM_TEST_SUITE_BEGIN(LDPCFunctionality)
 
 CSIM_TEST_CASE(CSV_Parse)
 {
-    cout << "hi2" << endl;
     CSVMatrix* p = new CSVMatrix();
 
+    vector<char> bytes;
+    bytes = p->loadCSVFile("data/ldpc/mat1.txt");
 
+    uint32_t rows, cols;
 
-   ifstream ifs("data/ldpc/mat1.txt", ios::binary|ios::ate);
-   ifstream::pos_type bytecount = ifs.tellg();
+    vector<vector<uint8_t> > H;
 
-   cout << "pos " << bytecount << endl;
+    p->parseCSV(rows, cols, bytes, H);
 
-   std::vector<char>  result(bytecount);
-   ifs.seekg(0, ios::beg);
-   ifs.read(&result[0], bytecount);
+    cout << "H rows " << H.size() << " H cols " << H[0].size() << endl;
 
-   uint32_t rows, cols;
+    cout << endl << endl;
 
-   vector<vector<uint8_t> > H;
-
-   p->parseSize(&rows, &cols, result, H);
-
-
-   cout << "H rows " << H.size() << " H cols " << H[0].size() << endl;
-
-   cout << endl << endl;
-
-
-
-   for(auto it = H.begin(); it != H.end(); ++it)
-   {
-       for(auto it2 = it->begin(); it2 != it->end(); ++it2)
-       {
-           cout << (int)*it2 << ",";
-       }
-//       cout << *it;
-       cout << endl;
-   }
-//
-//
-//   cout << endl;
-
-
-
-//   return result;
-
-
-
+    for (auto it = H.begin(); it != H.end(); ++it) {
+        for (auto it2 = it->begin(); it2 != it->end(); ++it2) {
+            cout << (int) *it2 << ",";
+        }
+        cout << endl;
+    }
 }
-
-
 
 CSIM_TEST_SUITE_END()
--- src/utils/ldpc_utils.hpp
+++ src/utils/ldpc_utils.hpp
@@ -24,7 +24,18 @@ public:
     CSVMatrix(){};
     ~CSVMatrix(){};
 
-    void parseSize(uint32_t *rows, uint32_t *cols, const std::vector<char>& input, vector<vector<uint8_t> >& out) const
+    std::vector<char> loadCSVFile(string filename) const
+    {
+        ifstream ifs(filename, ios::binary|ios::ate);
+        ifstream::pos_type bytecount = ifs.tellg();
+
+        std::vector<char> result(bytecount);
+        ifs.seekg(0, ios::beg);
+        ifs.read(&result[0], bytecount);
+        return result;
+    }
+
+    void parseCSV(uint32_t& rows, uint32_t& cols, const std::vector<char>& input, vector<vector<uint8_t> >& out) const
     {
         std::vector<char>::const_iterator eol = input.begin();  // end of previous line
 
@@ -35,38 +46,53 @@ public:
         {
             if(*it == '\n')
             {
-                cout << "count was " << count << endl;
                 string line;
                 line.assign(eol, it);
-                cout << "line was " << line << endl;
+//                cout << "line was " << line << endl;
                 std::vector<std::string> vec;
 
                 Tokenizer tok(line);
                 vec.assign(tok.begin(), tok.end());
 
-                cout << "vec size " << vec.size() << endl;
+                if(vec.size() == 1)
+                {
+                    continue;
+                }
 
-                cout << "vec contents:" << endl;
+//                cout << "vec size " << vec.size() << endl;
+//                cout << "vec contents:" << endl;
 
                 out.push_back(vector<uint8_t>());
 
                 for(auto it2 = vec.begin(); it2 != vec.end(); ++it2)
                 {
+//                    cout << "itr size " << it2->size() << endl;
+                    if( it2->size() > 0 )
+                    {
+                        out[linecount].push_back(atoi(it2->c_str()));
+                    }
 //                    cout << atoi(it2->c_str());
-                    out[linecount].push_back(atoi(it2->c_str()));
 //                    cout << *it2;
                 }
+//                cout << endl;
 
-                cout << endl;
+                if(linecount == 0)
+                {
+                    cols = (uint32_t) out[linecount].size();
+                }
+                else
+                {
+                    assert((uint32_t) out[linecount].size() == cols);
+                }
 
                 linecount++;
                 eol = it;
             }
-            cout << *it;
+//            cout << *it;
             count ++;
         }
 
-
+        rows = out.size();
     }
 
 //    template<int R, int C> void parseString(uint8_t H[R][C], const  std::vector<char>& input) const
