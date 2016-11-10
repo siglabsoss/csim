65b7c7956f22b825c15ba718f5157b5f49555cf1
Behrooze Sirang <behrooze@siglabs.com>
2016-11-10 11:58:44 -0800

    DDC: added DUC -> DDC loopback test

--- src/filters/utests/test_ddc.cpp
+++ src/filters/utests/test_ddc.cpp
@@ -127,6 +127,76 @@ static void runDUC(const std::string &up2CoeffFile, const std::string &up5CoeffF
     }
 }
 
+static void runLoopbackTest(const std::string &halfbandCoeffFile, const std::string &by5CoeffFile, const std::string &up2CoeffFile, const std::string &up5CoeffFile)
+{
+    std::vector<ComplexDouble> halfbandComplexCoeffs = utils::readComplexFromCSV<ComplexDouble>(halfbandCoeffFile);
+    std::vector<ComplexDouble> by5ComplexCoeffs      = utils::readComplexFromCSV<ComplexDouble>(by5CoeffFile);
+    std::vector<ComplexDouble> up2ComplexCoeffs      = utils::readComplexFromCSV<ComplexDouble>(up2CoeffFile);
+    std::vector<ComplexDouble> up5ComplexCoeffs      = utils::readComplexFromCSV<ComplexDouble>(up5CoeffFile);
+    assert(halfbandComplexCoeffs.size() != 0 && by5ComplexCoeffs.size() != 0);
+    assert(up2CoeffFile.size() != 0 && up5CoeffFile.size() != 0);
+
+    std::vector<double> halfbandCoeffs(halfbandComplexCoeffs.size());
+    std::vector<double> by5Coeffs(by5ComplexCoeffs.size());
+    std::vector<double> up2Coeffs(up2ComplexCoeffs.size());
+    std::vector<double> up5Coeffs(up5ComplexCoeffs.size());
+
+    for (size_t i = 0; i < halfbandComplexCoeffs.size(); i++) {
+        halfbandCoeffs[i] = halfbandComplexCoeffs[i].real();
+    }
+
+    for (size_t i = 0; i < by5ComplexCoeffs.size(); i++) {
+        by5Coeffs[i] = by5ComplexCoeffs[i].real();
+    }
+
+    for (size_t i = 0; i < up2ComplexCoeffs.size(); i++) {
+        up2Coeffs[i] = up2ComplexCoeffs[i].real();
+    }
+
+    for (size_t i = 0; i < up5ComplexCoeffs.size(); i++) {
+        up5Coeffs[i] = up5ComplexCoeffs[i].real();
+    }
+
+    DigitalDownConverter ddc(MIXER_FREQ, halfbandCoeffs, by5Coeffs);
+    DigitalUpConverter   duc(-MIXER_FREQ, up2Coeffs, up5Coeffs);
+
+    size_t numInputs = 1000;
+    SLFixedPoint<DUC_INPUT_FP_FORMAT> inph_in;
+    SLFixedPoint<DUC_INPUT_FP_FORMAT> quad_in;
+    filter_io_t input, output;
+    size_t outputCount = 0;
+    for (size_t ii = 0; ii < numInputs; ii++) {
+        inph_in = (ii & 1) ? 1.0 : -1.0;
+        quad_in = 0.0;
+
+        input.type = IO_TYPE_COMPLEX_FIXPOINT;
+        input.fc.setFormat(inph_in.wl(), inph_in.iwl());
+        input.fc.real(inph_in);
+        input.fc.imag(quad_in);
+
+//        std::cout << "in " << outputCount << ": " << input << std::endl;
+        duc.input(input);
+
+        for (size_t i = 0; i < 10; i++) {
+            duc.tick();
+            if (duc.output(output)) {
+//                std::cout << "duc: " << data << std::endl;
+                ddc.input(output);
+                ddc.tick();
+                if (ddc.output(output)) {
+//                    std::cout << "out " << outputCount << ": " << output << std::endl;
+                    outputCount++;
+                    //XXX compare magnitude/phase
+                    if (outputCount > 100) {
+                        BOOST_CHECK(std::abs(input.fc.real().to_double() - output.fc.real().to_double()) < 0.1);
+                        BOOST_CHECK(std::abs(input.fc.imag().to_double() - output.fc.imag().to_double()) < 0.1);
+                    }
+                }
+            }
+        }
+    }
+}
+
 CSIM_TEST_CASE(BASIC_DDC_FUNCTIONALITY)
 {
     //runDDC("./data/ddc/coeffs/halfband.txt", "./data/ddc/coeffs/downby5.txt", "./data/ddc/ddc_out.csv");
@@ -135,6 +205,11 @@ CSIM_TEST_CASE(BASIC_DDC_FUNCTIONALITY)
 CSIM_TEST_CASE(BASIC_DUC_FUNCTIONALITY)
 {
     //runDUC("./data/ddc/coeffs/halfband_interp.txt", "./data/ddc/coeffs/by5_interp.txt", "./data/ddc/ddc_out.csv", "./data/ddc/duc_out.csv");
+}
+
+CSIM_TEST_CASE(LOOPBACK_TEST)
+{
+    runLoopbackTest("./data/ddc/coeffs/halfband.txt", "./data/ddc/coeffs/downby5.txt", "./data/ddc/coeffs/halfband_interp.txt", "./data/ddc/coeffs/by5_interp.txt");
 }
 
 CSIM_TEST_SUITE_END()
