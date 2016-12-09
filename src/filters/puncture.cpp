702f984e1f5c45a74f2f29746f9b3e2948b81e41
Behrooze Sirang <behrooze@siglabs.com>
2016-12-09 14:39:02 -0800

    puncture: some code rearragement (non-behavior changing)

--- src/filters/puncture.cpp
+++ src/filters/puncture.cpp
@@ -4,19 +4,16 @@
 Puncture::Puncture(MCS mcs) :
         m_mcs(mcs),
         m_buffer(),
-        m_doPuncture(false),
+        m_doPuncture(mcs.getNumPuncOrRepBits() >= 0),
         m_fifo(mcs.getNumCodeWords() * mcs.getCodeWordLength()),
         m_outBitCount(0),
         m_outCodeWordCount(0)
 {
-    if (mcs.getNumPuncOrRepBits() < 0) { //we need to repeat bits instead of puncture
+    if (!m_doPuncture) { //we need to repeat bits instead of puncture
         size_t numRepeatBits = std::abs(m_mcs.getNumPuncOrRepBits());
         size_t numRepeatBitsPerSymbol = numRepeatBits / m_mcs.getNumOFDMSymbols();
         numRepeatBitsPerSymbol += 1; //one additional bit in case the division above truncated
         m_buffer = std::vector<bool>(numRepeatBitsPerSymbol);
-        m_doPuncture = false;
-    } else {
-        m_doPuncture = true;
     }
 }
 
@@ -28,13 +25,33 @@ bool Puncture::input(const filter_io_t &data)
     return true;
 }
 
+bool Puncture::output(filter_io_t &data)
+{
+    bool didOutput = false;
+    if (m_doPuncture) {
+        didOutput = handlePuncture(data);
+    } else { //repeating bits
+        didOutput = handleRepetition(data);
+    }
+    return didOutput;
+}
+
+void Puncture::tick()
+{
+
+}
+
 bool Puncture::handlePuncture(filter_io_t &data)
 {
+    bool didOutput = false;
     size_t numPunctureBits = m_mcs.getNumPuncOrRepBits();
     size_t numPunctureBitsPerSymbol = numPunctureBits / m_mcs.getNumOFDMSymbols();
-    bool didOutput = false;
     if (m_outCodeWordCount < (numPunctureBits % m_mcs.getNumOFDMSymbols())) {
         numPunctureBitsPerSymbol += 1;
+    }
+
+    if (m_fifo.empty()) {
+        return false;
     }
 
     if (m_outBitCount < m_mcs.getCodeWordLength() - numPunctureBitsPerSymbol) {
@@ -60,9 +77,9 @@ bool Puncture::handlePuncture(filter_io_t &data)
 
 bool Puncture::handleRepetition(filter_io_t &data)
 {
+    bool   didOutput = false;
     size_t numRepeatBits = std::abs(m_mcs.getNumPuncOrRepBits());
     size_t numRepeatBitsPerSymbol = numRepeatBits / m_mcs.getNumOFDMSymbols();
-    bool   didOutput = false;
     if (m_outCodeWordCount < (numRepeatBits % m_mcs.getNumOFDMSymbols())) {
         numRepeatBitsPerSymbol += 1; //the first rem(numRepeatBits, numOFDMSymbols) get an extra repeat bit
     }
@@ -95,21 +112,3 @@ bool Puncture::handleRepetition(filter_io_t &data)
     }
     return didOutput;
 }
-
-bool Puncture::output(filter_io_t &data)
-{
-    bool didOutput = false;
-    if (m_doPuncture) {
-        didOutput = handlePuncture(data);
-    } else { //repeating bits
-        didOutput = handleRepetition(data);
-    }
-    return didOutput;
-}
-
-void Puncture::tick()
-{
-
-}
-
-
