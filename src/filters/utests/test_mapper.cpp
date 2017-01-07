a1b96dcd07e77d95677d5237dd99c61e4f8f3eab
Behrooze Sirang <behrooze@siglabs.com>
2017-01-06 16:29:20 -0800

    Fixed utests that broke after adding subcarrier mapper to MCS

--- src/filters/utests/test_mapper.cpp
+++ src/filters/utests/test_mapper.cpp
@@ -16,6 +16,7 @@ CSIM_TEST_CASE(MAPPER_DOES_NOT_OUTPUT_BEFORE_FIRST_SYMBOL)
     MCS mcs(MCS::FIVE_SIXTHS_RATE, MCS::MOD_BPSK, 0);
     Mapper mapper(mcs);
     filter_io_t output;
+
     for (int i = 0; i < 100; i++) {
         mapper.tick();
         BOOST_CHECK(!mapper.output(output));
@@ -24,13 +25,14 @@ CSIM_TEST_CASE(MAPPER_DOES_NOT_OUTPUT_BEFORE_FIRST_SYMBOL)
 
 CSIM_TEST_CASE(MAPPER_DOES_OUTPUT_CORRECT_BPSK_SYMBOLS)
 {
-    std::vector<bool> byte = {0,1,0,1,0,1,0,1};
+    std::vector<bool> byte = { 0, 1, 0, 1, 0, 1, 0, 1 };
     filter_io_t data, output;
     MCS mcs(MCS::FIVE_SIXTHS_RATE, MCS::MOD_BPSK, 0);
     Mapper mapper(mcs);
+
     for (size_t i = 0; i < byte.size(); i++) {
         data.type = IO_TYPE_BIT;
-        data.bit = byte[i];
+        data.bit  = byte[i];
         BOOST_CHECK_EQUAL(mapper.input(data), true);
     }
 
@@ -38,40 +40,44 @@ CSIM_TEST_CASE(MAPPER_DOES_OUTPUT_CORRECT_BPSK_SYMBOLS)
         mapper.tick();
         mapper.output(output);
         int bit_idx = i;
+
         if (byte[bit_idx]) {
-            BOOST_CHECK_CLOSE(output.toComplexDouble().real(),  1.0, DBL_EPSILON);
-            BOOST_CHECK_CLOSE(output.toComplexDouble().imag(),  0.0, DBL_EPSILON);
+            BOOST_CHECK_CLOSE(output.toComplexDouble().real(), 1.0,  DBL_EPSILON);
+            BOOST_CHECK_CLOSE(output.toComplexDouble().imag(), 0.0,  DBL_EPSILON);
         } else {
             BOOST_CHECK_CLOSE(output.toComplexDouble().real(), -1.0, DBL_EPSILON);
-            BOOST_CHECK_CLOSE(output.toComplexDouble().imag(),  0.0, DBL_EPSILON);
+            BOOST_CHECK_CLOSE(output.toComplexDouble().imag(), 0.0,  DBL_EPSILON);
         }
     }
 }
 
 CSIM_TEST_CASE(MAPPER_DOES_OUTPUT_CORRECT_QAM16_SYMBOLS)
 {
-    MCS mcs(MCS::FIVE_SIXTHS_RATE, MCS::MOD_QAM16, 0, 104);
+    MCS mcs(MCS::FIVE_SIXTHS_RATE, MCS::MOD_QAM16, 0, 104, std::vector<bool>(104,
+                                                                             true));
     Mapper mapper(mcs);
 
     std::vector<bool> testData = {
-            0,0,0,1,
-            0,0,0,0,
-            0,0,1,1,
-            0,0,1,0,
-            0,1,0,1,
-            0,1,0,0,
-            0,1,1,1,
-            0,1,1,0,
-            1,0,0,1,
-            1,0,0,0,
-            1,0,1,1,
-            1,0,1,0,
-            1,1,0,1,
-            1,1,0,0,
-            1,1,1,1,
-            1,1,1,0};
+        0, 0, 0, 1,
+        0, 0, 0, 0,
+        0, 0, 1, 1,
+        0, 0, 1, 0,
+        0, 1, 0, 1,
+        0, 1, 0, 0,
+        0, 1, 1, 1,
+        0, 1, 1, 0,
+        1, 0, 0, 1,
+        1, 0, 0, 0,
+        1, 0, 1, 1,
+        1, 0, 1, 0,
+        1, 1, 0, 1,
+        1, 1, 0, 0,
+        1, 1, 1, 1,
+        1, 1, 1, 0
+    };
 
     filter_io_t data, output;
+
     for (unsigned int i = 0; i < testData.size(); i++) {
         data.type = IO_TYPE_BIT;
         data.bit  = testData[testData.size() - 1 - i];
@@ -80,18 +86,21 @@ CSIM_TEST_CASE(MAPPER_DOES_OUTPUT_CORRECT_QAM16_SYMBOLS)
 
     constellation_map_t expectedConstellations =  Mapper::getQAM16Constellations();
 
-    //These are the expected symbols for the test data.
-    std::vector<int> symbols = {1,0,3,2,5,4,7,6,9,8,11,10,13,12,15,14};
+    // These are the expected symbols for the test data.
+    std::vector<int> symbols =
+    { 1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14 };
 
     for (unsigned int i = 0; i < testData.size() / 4; i++) {
         mapper.tick();
         mapper.output(output);
 
-        int symbol_idx = 15 - (i);
-        int symbol = symbols[symbol_idx];
+        int symbol_idx          = 15 - (i);
+        int symbol              = symbols[symbol_idx];
         constellation_t current = expectedConstellations[symbol];
-        BOOST_CHECK_CLOSE(output.toComplexDouble().real(),  current.real().to_double(), DBL_EPSILON);
-        BOOST_CHECK_CLOSE(output.toComplexDouble().imag(),  current.imag().to_double(), DBL_EPSILON);
+        BOOST_CHECK_CLOSE(output.toComplexDouble().real(),
+                          current.real().to_double(), DBL_EPSILON);
+        BOOST_CHECK_CLOSE(output.toComplexDouble().imag(),
+                          current.imag().to_double(), DBL_EPSILON);
     }
 }
 
