257f21df311f5b899a1fd8043487b297771cc71f
Behrooze Sirang <behrooze@siglabs.com>
2017-01-06 17:57:26 -0800

    filter probe: increasing precision to maximum for CSV output

--- src/core/filter_probe.cpp
+++ src/core/filter_probe.cpp
@@ -1,8 +1,11 @@
 
 #include <core/filter_probe.hpp>
 #include <fstream>
+#include <iomanip>
 
-FilterProbe::FilterProbe(const std::string &name, size_t numElements, data_dump_t outType) :
+FilterProbe::FilterProbe(const std::string& name,
+                         size_t             numElements,
+                         data_dump_t        outType) :
     FilterChainElement(name),
     m_validInput(false),
     m_didReceiveNonzero(false),
@@ -11,10 +14,10 @@ FilterProbe::FilterProbe(const std::string &name, size_t numElements, data_dump_
     m_didTrigger(false),
     m_outType(outType),
     m_samplesSinceTrigger(0)
-{
-}
+{}
 
-FilterProbe::FilterProbe(const std::string &name, size_t numElements) :
+FilterProbe::FilterProbe(const std::string& name,
+                         size_t             numElements) :
     FilterChainElement(name),
     m_validInput(false),
     m_didReceiveNonzero(false),
@@ -23,35 +26,38 @@ FilterProbe::FilterProbe(const std::string &name, size_t numElements) :
     m_didTrigger(false),
     m_outType(NPLOT),
     m_samplesSinceTrigger(0)
-{
-}
+{}
 
 FilterProbe::~FilterProbe()
-{
+{}
 
-}
-
-bool FilterProbe::input(const filter_io_t &data)
+bool FilterProbe::input(const filter_io_t& data)
 {
     if (!m_didReceiveNonzero) {
-        switch(data.type) {
-            case IO_TYPE_COMPLEX_DOUBLE:
-                m_didReceiveNonzero =  (std::abs(data.rf) > 0.0);
-                break;
-            case IO_TYPE_COMPLEX_FIXPOINT:
-                m_didReceiveNonzero = (data.fc.real().to_int64() != 0) || (data.fc.imag().to_int64() != 0);
-                break;
-            case IO_TYPE_FIXPOINT:
-                m_didReceiveNonzero = (data.fp.to_int64() != 0);
-                break;
-            case IO_TYPE_BYTE:
-                m_didReceiveNonzero = (data.byte != 0);
-                break;
-            case IO_TYPE_BIT:
-                m_didReceiveNonzero = (data.bit != 0);
-                break;
-            case IO_TYPE_NULL:
-                break;
+        switch (data.type) {
+        case IO_TYPE_COMPLEX_DOUBLE:
+            m_didReceiveNonzero =  (std::abs(data.rf) > 0.0);
+            break;
+
+        case IO_TYPE_COMPLEX_FIXPOINT:
+            m_didReceiveNonzero = (data.fc.real().to_int64() != 0) ||
+                                  (data.fc.imag().to_int64() != 0);
+            break;
+
+        case IO_TYPE_FIXPOINT:
+            m_didReceiveNonzero = (data.fp.to_int64() != 0);
+            break;
+
+        case IO_TYPE_BYTE:
+            m_didReceiveNonzero = (data.byte != 0);
+            break;
+
+        case IO_TYPE_BIT:
+            m_didReceiveNonzero = (data.bit != 0);
+            break;
+
+        case IO_TYPE_NULL:
+            break;
         }
     }
     m_validInput = true;
@@ -59,14 +65,15 @@ bool FilterProbe::input(const filter_io_t &data)
     return true;
 }
 
-bool FilterProbe::output(filter_io_t &data)
+bool FilterProbe::output(filter_io_t& data)
 {
     if (m_didTrigger) {
         dump();
         m_didTrigger = false;
     }
+
     if (m_validInput) {
-        data = getLatest();
+        data         = getLatest();
         m_validInput = false;
         return true;
     }
@@ -77,23 +84,26 @@ bool FilterProbe::output(filter_io_t &data)
 void FilterProbe::dump()
 {
     switch (m_outType) {
-        case NPLOT:
-            return nplot();
-        case NPLOTFFT:
-            nplotfft();
-            break;
-        case NPLOTQAM:
-            nplotqam();
-            break;
-        case CSV:
-            csv();
-            break;
+    case NPLOT:
+        return nplot();
+
+    case NPLOTFFT:
+        nplotfft();
+        break;
+
+    case NPLOTQAM:
+        nplotqam();
+        break;
+
+    case CSV:
+        csv();
+        break;
     }
 }
 
 const filter_io_t& FilterProbe::getLatest() const
 {
-    return m_history[m_history.size()-1];
+    return m_history[m_history.size() - 1];
 }
 
 size_t FilterProbe::getSize() const
@@ -101,47 +111,56 @@ size_t FilterProbe::getSize() const
     return m_history.size();
 }
 
-void FilterProbe::trigger(const std::string &title)
+void FilterProbe::trigger(const std::string& title)
 {
     std::stringstream ss;
+
     ss << m_name << "_" << title;
     m_triggerName = ss.str();
-    m_didTrigger = true;
+    m_didTrigger  = true;
 }
 
 void FilterProbe::nplot()
 {
     m_p.nplot(m_history, getName() + " - " + m_triggerName);
 }
+
 void FilterProbe::nplotfft()
 {
     m_p.nplotfft(m_history, getName() + " - " + m_triggerName);
 }
+
 void FilterProbe::nplotqam()
 {
     m_p.nplotqam(m_history, getName() + " - " + m_triggerName);
 }
+
 void FilterProbe::csv()
 {
     std::ofstream ofs;
     std::stringstream filename;
+
     filename << m_triggerName << ".csv";
     ofs.open(filename.str().c_str(), std::ofstream::out | std::ofstream::trunc);
+    ofs << std::setprecision(52);
+
     for (auto it = m_history.begin(); it != m_history.end(); ++it) {
-        switch(it->type) {
-            case IO_TYPE_COMPLEX_DOUBLE:
-            case IO_TYPE_COMPLEX_FIXPOINT:
-            {
-                ComplexDouble val = it->toComplexDouble();
-                ofs << val.real() << "," << val.imag() << std::endl;
-                break;
-            }
-            case IO_TYPE_NULL:
-            case IO_TYPE_FIXPOINT:
-            case IO_TYPE_BYTE:
-            case IO_TYPE_BIT:
-            default:
-                ofs << *it;
+        switch (it->type) {
+        case IO_TYPE_COMPLEX_DOUBLE:
+        case IO_TYPE_COMPLEX_FIXPOINT:
+        {
+            ComplexDouble val = it->toComplexDouble();
+            ofs << val.real() << "," << val.imag() <<
+            std::endl;
+            break;
+        }
+
+        case IO_TYPE_NULL:
+        case IO_TYPE_FIXPOINT:
+        case IO_TYPE_BYTE:
+        case IO_TYPE_BIT:
+        default:
+            ofs << *it;
         }
     }
 }
