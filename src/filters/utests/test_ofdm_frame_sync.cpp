f9766aaeea69b377e225765f43e587e828c02d07
Behrooze Sirang <behrooze@siglabs.com>
2017-01-09 17:07:36 -0800

    OFDM frame sync: small tweaks to test routine

--- src/filters/utests/test_ofdm_frame_sync.cpp
+++ src/filters/utests/test_ofdm_frame_sync.cpp
@@ -1,40 +1,39 @@
 #include <test/unit_test.hpp>
 
 #include <filters/ofdm_frame_sync.hpp>
-#include <filters/fft.hpp>
+#include <filters/ddc_duc/ddc.hpp>
 #include <utils/utils.hpp>
-
+#include <iomanip>
 
 CSIM_TEST_SUITE_BEGIN(validateOFDMFrameSync)
 
-CSIM_TEST_CASE(FOO_BAR)
+// XXX make actual assertions
+CSIM_TEST_CASE(DID_DETECT_PILOT_AND_PASS_FRAME)
 {
     std::vector<ComplexDouble> inputs = utils::readComplexFromCSV<ComplexDouble>(
-        "./data/frame_sync/preamble_10db_snr.csv");
-    std::vector<ComplexDouble> inputs2(inputs.size() + 500, 0.0);
+        "./data/frame_sync/preamble_10db_43frames.csv");
 
-    for (size_t i = 0; i < inputs.size(); ++i) {
-        inputs2[500 + i] = inputs[i];
-    }
+    std::cout << std::setprecision(52);
 
     MCS mcs(MCS::ONE_HALF_RATE, MCS::MOD_BPSK, 3888, 1024);
     OFDMFrameSync fs(100, mcs);
 
     filter_io_t sample;
-    sample.type = IO_TYPE_COMPLEX_FIXPOINT;
-    sample.fc.setFormat(FFT_INPUT_FORMAT);
 
     for (size_t i = 0; i < inputs.size(); ++i) {
-        sample.fc.real(inputs2[i].real());
-        sample.fc.imag(inputs2[i].imag());
+        sample.type = IO_TYPE_COMPLEX_FIXPOINT;
+        sample.fc.setFormat(DDC_OUTPUT_FP_FORMAT);
+        sample.fc.real(inputs[i].real());
+        sample.fc.imag(inputs[i].imag());
+
+        // std::cout << i << " - " << inputs[i] << " ---- " << sample <<
+        // std::endl;
         fs.input(sample);
-        fs.tick();
-        fs.output(sample);
-    }
 
-    for (size_t i = 0; i < inputs.size(); ++i) {
-        fs.tick();
-        fs.output(sample);
+        for (size_t j = 0; j < 10; ++j) {
+            fs.tick();
+            fs.output(sample);
+        }
     }
 }
 
