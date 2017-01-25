030cedac9d22622fcdf42c34cc192c316791e6fc
Behrooze Sirang <behrooze@siglabs.com>
2017-01-25 12:55:58 -0800

    LDPC: reduce LLR format to Q6.2

--- src/filters/demapper.hpp
+++ src/filters/demapper.hpp
@@ -5,28 +5,35 @@
 #include <utils/plotter.hpp>
 #include <queue>
 
-#define SD_LLR_FORMAT             18, 8,  SLFixPoint::QUANT_RND_HALF_UP, SLFixPoint::OVERFLOW_SATURATE
+#define SD_LLR_FORMAT             8, 6,  SLFixPoint::QUANT_RND_HALF_UP, \
+    SLFixPoint::OVERFLOW_SATURATE
 
 class Demapper : public FilterChainElement
 {
 public:
-    Demapper(MCS mcs, bool hard = false);
+
+    Demapper(MCS  mcs,
+             bool hard = false);
     ~Demapper();
 
-    bool input(const filter_io_t &data) override;
+    bool input(const filter_io_t& data) override;
 
-    bool output(filter_io_t &data) override;
+    bool output(filter_io_t& data) override;
 
     void tick(void) override;
 
 private:
-    static double calcLLRIncrement(const ComplexDouble &rxSymbol, const ComplexDouble &constellation, double variance);
 
-    filter_io_t                 m_value;
-    bool                        m_inputValid;
-    constellation_map_t         m_constellations; //mapping of symbol -> constellation vector
-    size_t                      m_bitsPerSymbol;
-    bool                        m_hard; //hard decision (vs. soft decision)
-    double                      m_awgnVariance;
-    std::queue<double>          m_llrs;
+    static double calcLLRIncrement(const ComplexDouble& rxSymbol,
+                                   const ComplexDouble& constellation,
+                                   double               variance);
+
+    filter_io_t m_value;
+    bool m_inputValid;
+    constellation_map_t m_constellations; // mapping of symbol -> constellation
+                                          // vector
+    size_t m_bitsPerSymbol;
+    bool   m_hard;                        // hard decision (vs. soft decision)
+    double m_awgnVariance;
+    std::queue<double> m_llrs;
 };
