6ab387df553ef7c035efaf02ea08b606fb8094a8
Behrooze Sirang <behrooze@siglabs.com>
2017-01-25 16:29:56 -0800

    subcarrier mapper: generic way of generating training symbols based on a few input parameters

--- src/filters/subcarrier_mapper.hpp
+++ src/filters/subcarrier_mapper.hpp
@@ -16,17 +16,25 @@ public:
         OUTPUT_PREAMBLE,
         OUTPUT_SYMBOLS
     };
-    SubcarrierMapper(MCS mcs);
+    SubcarrierMapper(MCS    mcs,
+                     size_t pilotSpacing,
+                     size_t initialPilotOfset,
+                     size_t symbolRep);
     bool output(filter_io_t& data) override;
     void tick() override;
 
 private:
 
     void         initializePreamble();
+    size_t       getNumPilotTones() const;
 
     SLFixComplex getNextPreambleSymbol(bool& finished);
+    size_t       subcarrierIdxToFFTIdx(ssize_t subcarrierIdx) const;
 
     MCS m_mcs;
+    size_t  m_pilotSpacing;
+    size_t  m_initialPilotOffset;
+    size_t  m_symbolRep;
     state_t m_state;
     filter_io_t m_output;
     bool m_shouldOutput;
--- src/filters/utests/test_subcarrier_mapper.cpp
+++ src/filters/utests/test_subcarrier_mapper.cpp
@@ -1,7 +1,43 @@
 #include <test/unit_test.hpp>
 
+#define private public
 #include <filters/subcarrier_mapper.hpp>
+#undef private
+
 #include <filters/mapper.hpp>
+
+static std::vector<ComplexDouble>generatedExpectedPreamble(
+    const SubcarrierMapper& sm)
+{
+    std::vector<ComplexDouble> expectedPreambleTones(1024);
+
+    // The preamble symbols are generated using this PRNG with the default seed.
+    // Using the same
+    // generator here so that we generate the same sequence for testing.
+    // Exploiting this implementation
+    // detail is not ideal, but it's OK for now because we're likely to change
+    // the PN sequence for the
+    // preamble. This is to move us forward for now.
+    auto g =
+        std::bind(std::uniform_int_distribution<unsigned>(0, 1), std::mt19937());
+    size_t numPilots  = sm.getNumPilotTones();
+    double powerScale = sqrt(expectedPreambleTones.size() / numPilots);
+
+    for (size_t n = 0; n < expectedPreambleTones.size() / 2;
+         n += 2) {
+        // Positive tone
+        size_t fftIdx = sm.subcarrierIdxToFFTIdx(n);
+        int    val    = (g() << 1) - 1;
+        expectedPreambleTones[fftIdx].real(static_cast<double>(val) * powerScale);
+
+        // Corresponding negative tone
+        fftIdx = sm.subcarrierIdxToFFTIdx(-n);
+        val    = (g() << 1) - 1;
+        expectedPreambleTones[fftIdx].real(static_cast<double>(val) * powerScale);
+    }
+
+    return expectedPreambleTones;
+}
 
 CSIM_TEST_SUITE_BEGIN(validateSubcarrierMapper)
 
@@ -14,8 +50,13 @@ CSIM_TEST_CASE(OUTPUTS_PREAMBLE_BEFORE_SYMBOLS)
     for (size_t i = (1024 - 900) / 2; i < 1024 - ((1024 - 900) / 2); i++) {
         subMask[i] = true;
     }
+
+    size_t numPilotSymbols = 10;
+
     MCS mcs(MCS::FIVE_SIXTHS_RATE, MCS::MOD_QAM16, 4000, 1024, subMask);
-    SubcarrierMapper sm(mcs);
+    SubcarrierMapper sm(mcs, 2 /* pilot spacing */,
+                        0 /* initial pilot offset */,
+                        numPilotSymbols /* number of repetitions */);
 
     constellation_map_t constellations = Mapper::getQAM16Constellations();
 
@@ -27,46 +68,11 @@ CSIM_TEST_CASE(OUTPUTS_PREAMBLE_BEFORE_SYMBOLS)
                                    mcs.getNumOFDMSymbols();
 
     // Both preambles are as long as the number of subcarriers each.
-    const size_t totalPreambleLength = 2 * mcs.getNumSubCarriers();
+    const size_t totalPreambleLength = numPilotSymbols * mcs.getNumSubCarriers();
     filter_io_t  sample;
 
-    // The preamble symbols are generated using this PRNG with the default seed.
-    // Using the same
-    // generator here so that we generate the same sequence for testing.
-    // Exploiting this implementation
-    // detail is not ideal, but it's OK for now because we're likely to change
-    // the PN sequence for the
-    // preamble. This is to move us forward for now.
-    auto g =
-        std::bind(std::uniform_int_distribution<unsigned>(0, 1), std::mt19937());
-
-    std::vector<ComplexDouble> expectedPreambleTones(totalPreambleLength);
-
-    for (size_t i = 0; i < totalPreambleLength / 2; ++i) {
-        // The first symbol repeats twice in time within an OFDM
-        // symbol. This is because we loaded every
-        // second subchannel with a non-zero value
-        if (i % 2 == 0) {
-            int val = (g() << 1) - 1;
-            expectedPreambleTones[i].real(static_cast<double>(val) * M_SQRT2);
-        } else {
-            expectedPreambleTones[i].real(0.0);
-        }
-        expectedPreambleTones[i].imag(0.0);
-    }
-
-    for (size_t i = totalPreambleLength / 2; i < totalPreambleLength; ++i) {
-        // The second symbol repeats four times in time within an
-        // OFDM symbol. This is because we loaded every
-        // fourth subchannel with a non-zero value
-        if (i % 4 == 0) {
-            int val = (g() << 1) - 1;
-            expectedPreambleTones[i].real(static_cast<double>(val) * 2.0);
-        } else {
-            expectedPreambleTones[i].real(0.0);
-        }
-        expectedPreambleTones[i].imag(0.0);
-    }
+    std::vector<ComplexDouble> expectedPreambleTones = generatedExpectedPreamble(
+        sm);
 
     for (size_t i = 0; i < 4; ++i) {
         size_t numOutputs     = 0;
@@ -89,11 +95,13 @@ CSIM_TEST_CASE(OUTPUTS_PREAMBLE_BEFORE_SYMBOLS)
                 if (numOutputs < totalPreambleLength) {
                     BOOST_CHECK_CLOSE(
                         sample.fc.real().to_double(),
-                        expectedPreambleTones[numOutputs].real(),
+                        expectedPreambleTones[numOutputs %
+                                              expectedPreambleTones.size()].real(),
                         1.0 / (1 << 10));
                     BOOST_CHECK_CLOSE(
                         sample.fc.imag().to_double(),
-                        expectedPreambleTones[numOutputs].imag(),
+                        expectedPreambleTones[numOutputs %
+                                              expectedPreambleTones.size()].imag(),
                         1.0 / (1 << 10));
                 } else {
                     // symbol outputs
