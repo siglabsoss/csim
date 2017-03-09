29b661d0a1b2de81fa7f32dfc8112ddef2088a55
Behrooze Sirang <behrooze@siglabs.com>
2017-03-09 15:36:36 -0800

    timing_alignment: create dummy app to use for timing alignment development

---
+++ src/apps/timing_alignment.cpp
@@ -0,0 +1,39 @@
+#include <utils/utils.hpp>
+
+static void runFilter(const std::vector<ComplexDouble>& inputs)
+{
+    printf("Running filter with %ld input samples\n", inputs.size());
+
+    // Write filter here
+}
+
+int main(int argc, char *argv[])
+{
+    int  c;
+    bool gotInputFile = false;
+
+    std::string inputFile;
+
+    while ((c = getopt(argc, argv, "f:")) != -1) {
+        switch (c) {
+        case 'f':
+            inputFile    = std::string(optarg);
+            gotInputFile = true;
+            break;
+        }
+    }
+
+    if (gotInputFile != true) {
+        std::cout << "Usage: " << argv[0] << " -f <input filename>" << std::endl;
+        return 1;
+    }
+
+    std::vector<ComplexDouble> inputs = utils::readComplexFromCSV<ComplexDouble>(
+        inputFile);
+
+    assert(inputs.size() > 0);
+
+    runFilter(inputs);
+
+    return 0;
+}
