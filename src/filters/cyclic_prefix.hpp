3ee24f227c4320c65c201d95439601387a7d17a0
Behrooze Sirang <behrooze@siglabs.com>
2017-04-10 15:24:16 -0700

    cyclic_prefix: add documentation

--- src/filters/cyclic_prefix.hpp
+++ src/filters/cyclic_prefix.hpp
@@ -1,3 +1,9 @@
+/**
+ * @file cyclic_prefix.hpp
+ *
+ * A cyclic prefix generator.
+ */
+
 #pragma once
 
 #include <types/circularbuffer.hpp>
@@ -5,13 +11,36 @@
 #include <utils/mcs.hpp>
 #include <vector>
 
+/**
+ * A cyclic prefix generator.
+ *
+ * This filter outputs a copy of its input with cpLen samples prepended to it.
+ * These cpLen samples are a copy of the last cpLen samples of the input.
+ * See https://en.wikipedia.org/wiki/Cyclic_prefix for more information.
+ */
 class CyclicPrefix : public FilterChainElement
 {
 public:
-    CyclicPrefix(size_t N, size_t cpLen, size_t ticksPerOutput, MCS mcs);
-    bool output(filter_io_t &data) override;
+
+    /**
+     * Construct a cyclic prefix generator.
+     * @param N the number of input samples. This is typically the same as the
+     * FFT size in an OFDM system.
+     * @param cpLen the number of samples to prefix. This must be less than N
+     * and is typically about one-eigth of N.
+     * @param ticksPerOutput used to throttle the rate of the output.
+     * @param MCS the selected modulation and coding scheme, which is used
+     * to set a sensible FIFO size.
+     */
+    CyclicPrefix(size_t N,
+                 size_t cpLen,
+                 size_t ticksPerOutput,
+                 MCS    mcs);
+    bool output(filter_io_t& data) override;
     void tick(void) override;
+
 private:
+
     size_t m_len;
     size_t m_symbolLen;
     size_t m_inputIdx;
