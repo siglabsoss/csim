d5e01b1128bbaf373efc9d7cbbd1cba74f007d7d
Behrooze Sirang <behrooze@siglabs.com>
2017-04-10 15:24:44 -0700

    puncture: add documentation

--- src/filters/puncture.hpp
+++ src/filters/puncture.hpp
@@ -1,3 +1,6 @@
+/**
+ * @file Perform puncturing and/or repetition for LDPC encoding.
+ */
 #pragma once
 
 #include <core/filter_chain_element.hpp>
@@ -5,20 +8,29 @@
 #include <utils/mcs.hpp>
 #include <vector>
 
+/**
+ * Perform puncturing. Puncturing/repetition is part of the LDPC encoding
+ * process for OFDM communication systems.
+ * More information about this process can be found starting at page 165 of
+ * Next Generation Wireless LANs. See:
+ **https://books.google.com/books?id=hJ1cAwAAQBAJ&lpg=PP1&dq=Next%20Generation%20Wireless%20LANs&pg=PA165#v=snippet&q=LDPC%20Encoding%20Process&f=false
+ */
 class Puncture : public FilterChainElement
 {
 public:
+
     Puncture(MCS mcs);
-    bool output(filter_io_t &data)      override;
+    bool output(filter_io_t& data)      override;
     void tick(void)                     override;
+
 private:
-    bool handlePuncture(filter_io_t &data);
-    bool handleRepetition(filter_io_t &data);
+
+    bool handlePuncture(filter_io_t& data);
+    bool handleRepetition(filter_io_t& data);
 
     MCS m_mcs;
     std::vector<bool> m_buffer;
-    bool m_doPuncture;
+    bool   m_doPuncture;
     size_t m_outBitCount;
     size_t m_outCodeWordCount;
 };
-
